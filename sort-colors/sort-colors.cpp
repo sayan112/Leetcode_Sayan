6bd81d11bab737a94065a4bf16c2ea8d2c7cdf6d
SAYAN MAITRA <75684139+sayan112@users.noreply.github.com>
2021-12-16 12:51:46 +0530

    Time: 6 ms (44.75%), Space: 8.3 MB (20.59%) - LeetHub

--- sort-colors/sort-colors.cpp
+++ sort-colors/sort-colors.cpp
@@ -1,8 +1,16 @@
 class Solution {
 public:
-    void sortColors(vector<int>& nums) {
-      
-         sort(nums.begin(),nums.end());
-         
+    void sortColors(vector<int>& vec) {
+          int lo=0,mid=0,hi=vec.size()-1;
+        while(mid<=hi){
+            if(vec[mid]==0){
+                swap(vec[mid++],vec[lo++]);
+            }
+            else if(vec[mid]==1)
+                mid++;
+            else{
+                swap(vec[mid],vec[hi--]);
+            }
+        }
     }
 };
