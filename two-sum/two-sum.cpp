003bb28590cc227a0800129c21d19031931f6b0e
SAYAN MAITRA <75684139+sayan112@users.noreply.github.com>
2021-12-18 23:41:50 +0530

    Time: 12 ms (77.76%), Space: 12.1 MB (8.38%) - LeetHub

--- two-sum/two-sum.cpp
+++ two-sum/two-sum.cpp
@@ -1,22 +1,54 @@
 class Solution {
 public:
+    // brute force ok :)
+    
     vector<int> twoSum(vector<int>& nums, int target) {
-      vector<int>need1;
-for (int i = 0; i <nums.size(); i++)
-{
-   for (int j =i+1; j <nums.size(); j++)
-   {
-       int need=target-nums[i];
-       if(need==nums[j])
-       {
-           need1.push_back(i);
-           need1.push_back(j);
-           
-       }
-   }
-   
-}
-            return need1;
   
+// for (int i = 0; i <nums.size(); i++)
+// {
+//    for (int j =i+1; j <nums.size(); j++)
+//    {
+//        int need=target-nums[i];
+//        if(need==nums[j])
+//        {
+//            need1.push_back(i);
+//            need1.push_back(j);
+           
+//        }
+//    }
+   
+// }
+//             return need1;
+  
+//     }
+    
+//      solution op 
+    
+    unordered_map<int,int>mp;   
+        vector<int>need1;
+
+    
+     for(int i = 0; i<nums.size();i++)
+     {
+          int need = target-nums[i];
+         if(mp.find(need)!=mp.end())
+         {
+            need1.push_back(i);
+             need1.push_back(mp[need]);
+         }
+         
+               mp[nums[i]] = i;
+     }
+     return  need1;
+    
+    
+
+    
+    
     }
+    
+    
+    
+    
+    
 };
