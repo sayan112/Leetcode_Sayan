7490ebff50036d1145f4d7a5cd962d0b948af31a
SAYAN MAITRA <75684139+sayan112@users.noreply.github.com>
2021-12-22 16:15:31 +0530

    Time: 4 ms (75.12%), Space: 12.1 MB (73.15%) - LeetHub

--- next-permutation/next-permutation.cpp
+++ next-permutation/next-permutation.cpp
@@ -1,6 +1,49 @@
 class Solution {
 public:
-    void nextPermutation(vector<int>& nums) {
-       next_permutation(nums.begin(),nums.end());
+    void nextPermutation(vector<int>& vec) {
+       // next_permutation(nums.begin(),nums.end());
+         
+int i;
+ for ( i = vec.size()-2; i>=0; i--)
+ {
+     if(vec[i]<vec[i+1])
+     {
+         break;
+     }
+ }
+          cout << i ; 
+  if(i<0)
+  {
+   reverse(vec.begin(),vec.end());
+
+  }
+
+  else 
+  {
+       for (int  j = vec.size()-1; j>0; j--)
+ {
+     
+      if(vec[j]>vec[i])
+      {
+          swap(vec[j],vec[i]);
+          break;
+      }
+ }
+
+
+
+// for(auto elem : vec)
+// {
+//     cout <<elem <<" "
+// ;}
+
+// cout <<endl;
+ reverse(vec.begin()+i+1, vec.end());
+
+// for(auto elem : vec)
+// {
+//     cout <<elem <<" "
+// ;}
+    }
     }
 };
