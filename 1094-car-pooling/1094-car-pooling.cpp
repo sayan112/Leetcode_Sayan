6225753cccbe5eea1573f1d94354bfbf77bd9fb2
SAYAN MAITRA <75684139+sayan112@users.noreply.github.com>
2022-01-08 01:08:42 +0530

    Time: 84 ms (5.86%), Space: 10.1 MB (71.40%) - LeetHub

--- 1094-car-pooling/1094-car-pooling.cpp
+++ 1094-car-pooling/1094-car-pooling.cpp
@@ -5,28 +5,40 @@ public:
 
        for (int i = 0; i < trips.size(); i++)
           {
-             
+         
+                int need1 =  trips[i][0];
+                int need2=  trips[i][1];
+                 int need3 = trips[i][2];
            
-                   arr[trips[i][1]]+=trips[i][0];
-                    arr[ trips[i][2]]-=trips[i][0];  
+                   arr[need2]+=need1;
+                    arr[need3]-=need1;
+
+                  
+                  
+
+             
              
           }
-         
+          
+        
+//           prefix sum 
         
        for (int i = 1; i <arr.size(); i++)
           {
         arr[i]+=arr[i-1];
        }
         
-         
-           if(capacity < *max_element(arr.begin(),arr.end()))
-        {
+            for(auto elem : arr)
+           {
+            cout << elem << " " ; 
+                 if(elem>capacity )
+                 {
+                      return false;
+                 }
+                
+           }
         
-                  return false;
-        }else{
-                return true;
-        }
-        
+         return true;
         
 
     
