202fdda149b01bdc7a71ae4934987bed13fecada
SAYAN MAITRA <75684139+sayan112@users.noreply.github.com>
2022-01-14 00:12:57 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (6.27%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -1,26 +1,21 @@
 class Solution {
 public:
  unordered_map<int,int>mp;
-int tribonacci(int n) {
-          
+int tribonacci(int n) {          
       if (n==0)
         {
             return 0  ;
         }
-    else if  (n==2 || n==1)
+     if  (n==2 || n==1)
     {
         return 1 ;
         
-    }
-        
+    }        
         if(mp.find(n)!=mp.end()){
             return mp[n];
         }
-    
-      
-    int ans= tribonacci(n-1)+tribonacci(n-2)+tribonacci(n-3);
-      mp[n] =  ans ;
-        return ans ;
+      mp[n] =  tribonacci(n-1)+tribonacci(n-2)+tribonacci(n-3);
+        return tribonacci(n-1)+tribonacci(n-2)+tribonacci(n-3) ;
     
 }
 };
