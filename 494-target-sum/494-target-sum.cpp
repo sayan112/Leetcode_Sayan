27c8275ad98ece5131b7d8c551cf7121d51e3be5
SAYAN MAITRA <75684139+sayan112@users.noreply.github.com>
2022-01-17 02:55:27 +0530

    Time: 1320 ms (10.91%), Space: 8.9 MB (92.04%) - LeetHub

--- 494-target-sum/494-target-sum.cpp
+++ 494-target-sum/494-target-sum.cpp
@@ -3,17 +3,19 @@ public:
     
      int  targetSum(vector<int>& nums,int currentIndx, int target)
      {
+               if( currentIndx >= nums.size() &&target != 0)
+            return 0;
+        
+      
              if(target == 0 &&  currentIndx==nums.size() )
             return 1;
-        
-            if( currentIndx >= nums.size())
-            return 0;
         
         int pick = targetSum(nums,  currentIndx+ 1, target - nums[ currentIndx]);
               int nonpick=targetSum(nums, currentIndx + 1, target + nums[ currentIndx]);
          
           return pick+nonpick;
      }
+    
     int findTargetSumWays(vector<int>& nums, int target) {
       
         return targetSum(nums, 0, target);
