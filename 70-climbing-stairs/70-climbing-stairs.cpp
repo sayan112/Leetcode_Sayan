3d2a6b135ee369843a49ba507c0d5be10e462a9f
SAYAN MAITRA <75684139+sayan112@users.noreply.github.com>
2022-01-16 20:46:38 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (14.56%) - LeetHub

---
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int totalWays(int targetStair, int currentStair,unordered_map<int,int>&mp){
+        if(currentStair == targetStair){
+            return 1;
+        }
+        
+
+        if(currentStair > targetStair){
+            return 0;
+        }
+
+        int currentKey = currentStair;
+	
+        if(mp.find(currentKey) != mp.end() ){
+            return mp[currentKey];
+        }
+        
+        int oneStep = totalWays(targetStair, currentStair+1,mp);
+        int twoStep = totalWays(targetStair, currentStair+2,mp);
+        
+        mp[currentKey] = oneStep + twoStep;
+        
+        return mp[currentKey];
+    }
+    
+    
+    int climbStairs(int n) {
+        unordered_map<int,int> mp;
+        return totalWays(n,0,mp);
+    }
+};
