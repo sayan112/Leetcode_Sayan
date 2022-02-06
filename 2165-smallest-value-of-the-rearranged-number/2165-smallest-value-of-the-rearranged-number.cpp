8f2e638f3b0efeab9964ce28bbe0956b2d1227bf
SAYAN MAITRA <75684139+sayan112@users.noreply.github.com>
2022-02-06 11:38:48 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (25.00%) - LeetHub

--- 2165-smallest-value-of-the-rearranged-number/2165-smallest-value-of-the-rearranged-number.cpp
+++ 2165-smallest-value-of-the-rearranged-number/2165-smallest-value-of-the-rearranged-number.cpp
@@ -1,136 +1,93 @@
-class Solution {
+class Solution
+{
 public:
-    long long smallestNumber(long long num) {
-         if(num==0)
-         {
-             return 0;
-         }
- if ( num >0)
- {
-     
-     
-     vector<int>need;
-     
-     while(num)
-     {
-         int i=num%10;
-         need.push_back(i);
-          num=num/10;
-     }
-   sort(need.begin(),need.end());
-//      for(auto elem : need)
-//      {
-//          cout << elem;
-//      }
-     
-     string m;
-     
-     if(need[0]!=0)
-     {
-         for(auto elem : need)
-         {
-              m+=elem+'0';
-         }
-         
-          long long  real= stoll(m);
-         cout << real;
-              
-     return  real;
-     }
-     else{
-         
-          int final=-1;
-         
-          for(int i=0; i < need.size(); i++)
-          {
-               if(need[i]==0)
-               {
-                   final=i;
-               }
-              else{
-                  break;
-              }
-          }
-         m+=need[final+1]+'0';
-         
-                for(int i=0; i <=final; i++)
-          {
-                    m+= need[i]+'0';
-             
-          }
-                  for(int i=final+2; i <need.size(); i++)
-          {
-                    m+= need[i]+'0';
-             
-          }
-              
-         long long  real= stoll(m);
-              
-     return  real;
-         
-         
-         
-         
-         
-         
-     }
-     
-     
-     
-     
-     
- }
-        else{
-            
-             // int  num =num*-1;
-             long long  mini= num *-1;
-            
-            cout << mini << endl ;
-                 vector<int>need;
-     
-     while(mini)
-     {
-         int i=mini%10;
-         // cout << i ;
-         need.push_back(i);
-         mini=mini/10;
-     }
-   sort(need.begin(),need.end(),greater<int>());
-     for(auto elem : need)
-     {
-         cout << elem;
-     }
-            
-            string m;
-            
-                     for(auto elem : need)
-         {
-              m+=elem+'0';
-         }
-         
-          long long real= stoll(m);
-         cout << real;
-              
-     return real*-1;
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-        }
-        
-        
-        
-        
-    }
-        
- 
+   long long smallestNumber(long long num)
+   {
+      if (num == 0)
+      {
+         return 0;
+      }
+      if (num > 0)
+      {
 
+         vector<int> need;
+
+         while (num)
+         {
+            int i = num % 10;
+            need.push_back(i);
+            num = num / 10;
+         }
+         sort(need.begin(), need.end());
+         string m;
+         if (need[0] != 0)
+         {
+            for (auto elem : need)
+            {
+               m += elem + '0';
+            }
+
+            long long real = stoll(m);
+
+            return real;
+         }
+         else
+         {
+
+            int zero = -1;
+
+            for (int i = 0; i < need.size(); i++)
+            {
+               if (need[i] == 0)
+               {
+                  zero = i;
+               }
+               else
+               {
+                  break;
+               }
+            }
+            m += need[zero + 1] + '0';
+
+            for (int i = 0; i <= zero; i++)
+            {
+               m += need[i] + '0';
+            }
+            for (int i = zero + 2; i < need.size(); i++)
+            {
+               m += need[i] + '0';
+            }
+
+            long long real = stoll(m);
+
+            return real;
+         }
+      }
+      else
+      {
+         long long minus = num * -1;
+
+         vector<int> need;
+
+         while (minus)
+         {
+            int i = minus % 10;
+            // cout << i ;
+            need.push_back(i);
+            minus = minus / 10;
+         }
+         sort(need.begin(), need.end(), greater<int>());
+
+         string m;
+
+         for (auto elem : need)
+         {
+            m += elem + '0';
+         }
+
+         long long real = stoll(m);
+
+         return real * -1;
+      }
+   }
 };
