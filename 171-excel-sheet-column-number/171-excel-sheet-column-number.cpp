064ff52c7083442b0bfbcd352313aa248195f0e8
SAYAN MAITRA <75684139+sayan112@users.noreply.github.com>
2022-02-22 13:16:43 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (13.67%) - LeetHub

--- 171-excel-sheet-column-number/171-excel-sheet-column-number.cpp
+++ 171-excel-sheet-column-number/171-excel-sheet-column-number.cpp
@@ -2,11 +2,13 @@ class Solution {
 public:
     int titleToNumber(string c) {
         int need=0;
+         int number= c.size()-1;
          for(auto elem : c)
          { 
               int num=((elem-'A')+1);
-             cout << num << endl;
-             need=need*26 +num;
+             need+= num*(pow(26,number)); 
+                         number--;
+             
               
          }
          return need;
