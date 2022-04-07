bc7c8cdf69c4600625a91d2a10c6e1c4dbf7ec17
SAYAN MAITRA <75684139+sayan112@users.noreply.github.com>
2022-04-07 15:45:22 +0530

    Time: 0 ms (100.00%), Space: 7.7 MB (34.27%) - LeetHub

---
+++ 1046-last-stone-weight/1046-last-stone-weight.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        priority_queue<int>max_heap;
+            int n = stones.size();
+         for(int i=0; i < stones.size();i++)
+         {
+             max_heap.push(stones[i]);
+         }
+        
+         
+    int first, second;
+        while(n>1)
+        {
+            first = max_heap.top();
+             max_heap.pop();
+            
+         second= max_heap.top();
+                max_heap.pop();
+            n--;
+            n--;
+             if(first-second>=1)
+             {
+                 int need = first- second;
+                 max_heap.push(need);
+                n++; 
+                 
+             }
+        
+            
+        }
+        
+         if(max_heap.empty())
+         {
+          return 0;
+         }
+        else{
+     return max_heap.top();
+        }
+    }
+};
