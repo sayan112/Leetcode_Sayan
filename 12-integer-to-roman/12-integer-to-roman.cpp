c05d410b81aa7005c735c510d767228976076126
SAYAN MAITRA <75684139+sayan112@users.noreply.github.com>
2022-04-15 11:59:31 +0530

    Time: 17 ms (30.10%), Space: 13 MB (8.18%) - LeetHub

---
+++ 12-integer-to-roman/12-integer-to-roman.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    string intToRoman(int num) {
+map<int,string> mp{{1,"I"},{4,"IV"},{5,"V"},{9,"IX"},{10,"X"},{40,"XL"},{50,"L"},{90,"XC"},{100,"C"},{400,"CD"},{500,"D"},{900,"CM"},{1000,"M"}};
+ 
+        string res="";
+          while(num!=0)
+        {
+            auto it=mp.upper_bound(num);
+            it--;
+            res+=(it->second);
+            num=num-(it->first);
+        }
+        
+        return res;
+        
+        
+        
+        
+        
+         
+        
+    }
+};
