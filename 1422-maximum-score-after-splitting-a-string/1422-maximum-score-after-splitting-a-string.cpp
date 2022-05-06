bfb230a26a227a0968ca2c2225ee0343c92e3595
SAYAN MAITRA <75684139+sayan112@users.noreply.github.com>
2022-05-06 21:19:04 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (97.00%) - LeetHub

--- 1422-maximum-score-after-splitting-a-string/1422-maximum-score-after-splitting-a-string.cpp
+++ 1422-maximum-score-after-splitting-a-string/1422-maximum-score-after-splitting-a-string.cpp
@@ -1,44 +1,63 @@
 class Solution {
 public:
-    
-    int noofzero(string s )
-    {
-         int zero=0;
-        for(auto elem : s)
-        {
-            if(elem=='0')
-            {
-            zero++;
-            }
-        }
-         return zero;
-    }
-        int noofone(string s )
-    {
-         int one=0;
-        for(auto elem : s)
-        {
-            if(elem=='1')
-            {
-            one++;
-            }
-        }
-         return one;
-    }
-    
     int maxScore(string s) {
-         int need =s.size(); 
-         int ans =0;
-        int maxi=0;
-         for(int i=0; i < s.size()-1;i++)
+         int noofone=0;
+           int noofzero=0;
+         for(auto elem : s )
          {
-             string s1=s.substr(0, i+1);
-             
-              string s2=s.substr(i+1, (need-i));
-            ans=noofone(s2)+noofzero(s1);
-               maxi=max(maxi,ans);   
+             if(elem=='1')
+             {
+                 noofone++;
+             }
          }
         
+         if(noofone==0)
+         {
+              return s.size()/2;
+         }
+   
+      
+        int maxi=0;
+         for(int i=0; i<s.length()-1; i++){
+             if(s[i]=='0')
+             {
+                 noofzero++;
+             }
+              else{
+                  noofone--;
+              }
+              int  score= noofone+noofzero;
+               maxi=max(maxi,score);
+         }
          return maxi;
+        
+        
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+// class Solution {
+// public:
+//     int maxScore(string s) {
+//         int rightOnes = 0, leftZeroes = 0;
+//         for(char c: s)
+//             if(c=='1') rightOnes++;
+        
+//         int score = 0;
+//         for(int i=0; i<s.length()-1; i++){
+//             if(s[i]=='0') leftZeroes++;
+//             else rightOnes--;
+//             score = max(score, leftZeroes + rightOnes);
+//         }
+//         return score;
+//     }
+// };
