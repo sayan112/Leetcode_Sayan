4c38bff84f9db264ff25833814f0518c3c48261a
SAYAN MAITRA <75684139+sayan112@users.noreply.github.com>
2022-05-08 02:32:28 +0530

    Time: 24 ms (23.92%), Space: 18.5 MB (36.94%) - LeetHub

--- 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.cpp
+++ 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.cpp
@@ -1,22 +1,7 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        
-         // But here we are taking extra space but we dont do it in interview :)
-        
-//         set<int>l;
-//        for(auto elem : nums)
-//        {
-//            l.insert(elem);
-//        }
-//             nums.clear();
-        
-//       for(auto elem : l)
-//       {
-//        nums.push_back(elem);
-//       }
-        
-//   return l.size();
+
         
          int slow =0;
          int fast =1;
@@ -30,6 +15,7 @@ public:
              fast++;
          }
          return slow+1;
+
         
     }
 };
