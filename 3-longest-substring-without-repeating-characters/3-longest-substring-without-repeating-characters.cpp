0522a821979f4b5c64bbc54d9972fef56c366763
SAYAN MAITRA <75684139+sayan112@users.noreply.github.com>
2022-05-07 21:21:40 +0530

    Time: 22 ms (56.25%), Space: 9 MB (38.27%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -1,40 +1,37 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-          int i=0;
-     int j=0;
-     unordered_map<char,int>mp;
-     int count =0;
-  
-
-while(j<s.size())
-{
-     mp[s[j]]++;
-     
-     if(mp.size()==j-i+1)
-     {
-       count=max(count, j-i+1);
-     }
- 
-     else if(mp.size()<j-i+1)
-     {
-         while (mp.size()<j-i+1)
-         {
-              mp[s[i]]--;
-               if(mp[s[i]]==0)
-               {
-                   mp.erase(s[i]);
-               }
-                 i++;
-         }
-       
-      
-         
-     }
-     j++;
-
-}
- return count ;
-
+        
+         int i=0;
+     int j =0;
+      unordered_map<char,int>mp;
+       int maxi =0;
+      while(j<s.size())
+       {
+                 mp[s[j]]++;
+                if((j-i+1)<mp.size())
+                {
+                    j++;
+                }
+                else if ((j-i+1)==mp.size()){
+                    maxi=max(maxi,(j-i+1));
+                    j++;
+                }
+                else{
+                    while((j-i+1)>mp.size())
+                    {
+                        mp[s[i]]--;
+                        if(mp[s[i]]==0)
+                        {
+                             mp.erase(s[i]);
+                        }
+                        i++;
+                    }
+                    j++;
+                }
+       }
+        return maxi;
+    
+    
     }
 };
