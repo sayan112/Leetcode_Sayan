461c0e116633c3ae0977268b268a2214fa5696e4
SAYAN MAITRA <75684139+sayan112@users.noreply.github.com>
2022-05-13 20:10:03 +0530

    Time: 312 ms (54.46%), Space: 69.6 MB (45.08%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.cpp
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.cpp
@@ -1,41 +1,26 @@
 class Solution {
 public:
     vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
-        
-      vector<int>ans1;
-         for(auto elem : points)
-         {
-              int need=0;
-              for(auto elem1 : elem)
-              {
-                  need+=elem1*elem1;
-              }
-             ans1.push_back(need);
-         }
-         for(auto elem : ans1)
-         {
-             cout << elem <<" ";
-         }
-        
+   
              priority_queue<pair<int, pair<int, int>>> pq;
-        for( int i=0; i < ans1.size();i++)
+        for( int i=0; i <points.size() ;i++)
         {
-            pq.push({ans1[i], {points[i][0],points[i][1]}});
+            pq.push({((points[i][0]*points[i][0])+ (points[i][1]*points[i][1])), {points[i][0],points[i][1]}});
             if(pq.size()>k)
             {
                 pq.pop();
             }
         }
-         vector<vector<int>>finalans;
         
-     
+        points.clear();
+    
          while(pq.size()!=0)
          {
                 vector<int>ans2;
              auto elem = pq.top().second;
              ans2.push_back(elem.first);
               ans2.push_back(elem.second);
-           finalans.push_back(ans2);
+           points.push_back(ans2);
              pq.pop();
              
          }
@@ -43,6 +28,6 @@ public:
         
         
         
-         return finalans ;
+         return points;
     }
 };
