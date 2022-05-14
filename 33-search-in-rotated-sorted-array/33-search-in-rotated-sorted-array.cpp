93ceeb107d90602e3cfaf6fda00bf1c8a3c9553c
SAYAN MAITRA <75684139+sayan112@users.noreply.github.com>
2022-05-14 15:41:56 +0530

    Time: 11 ms (14.22%), Space: 11.3 MB (29.74%) - LeetHub

---
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
@@ -0,0 +1,76 @@
+class Solution
+{
+    public:
+        int findMin(vector<int> &nums)
+        {
+            int start = 0;
+            int size = nums.size();
+            int end = nums.size() - 1;
+            if (nums[end] >= nums[start])
+            {
+                return nums[0];
+            }
+            else
+            {
+                while (end >= start)
+                {
+                    int mid = (end + start) / 2;
+                    int prev = (mid + nums.size() - 1) % size;
+                    int next = (mid + 1) % size;
+                    if (nums[mid] <= nums[prev] && nums[mid] <= nums[next])
+                    {
+                        return mid;
+                    }
+                    else if (nums[mid] > nums[end])
+                    {
+                        start = mid + 1;
+                    }
+                    else
+                    {
+                        end = mid;
+                    }
+                }
+            }
+            return -1;
+        }
+    int binearsearch(vector<int> nums,int target,int start, int end )
+    {
+        while(end>=start)
+        {
+             int mid = (end+start)/2;
+             if( nums[mid]==target)
+             {
+                  return mid ;
+             }
+             if(nums[mid]>target)
+             {
+                 end= mid-1;
+             }
+            else {
+                start = mid+1;
+            }
+        }
+        return -1;
+    }
+    int search(vector<int> &nums, int target) {
+        int min = findMin(nums);
+        cout << min <<endl;
+        int need1=  binearsearch(nums,target,0,min-1);
+     int need2=   binearsearch(nums,target,min,nums.size()-1);
+         if( need1==-1 && need2!=-1)
+         {
+              return need2 ;
+         }
+        else if(need2==-1 && need1!=-1)
+        {
+             return need1;
+        }
+        
+        return -1;
+        
+        
+        
+        
+        
+    }
+};
