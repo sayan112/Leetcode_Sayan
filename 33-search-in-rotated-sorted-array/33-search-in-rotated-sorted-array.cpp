72e1aa17102e7e7441b3fc95087094239595805a
SAYAN MAITRA <75684139+sayan112@users.noreply.github.com>
2022-05-14 15:42:17 +0530

    Time: 8 ms (31.02%), Space: 11.4 MB (29.74%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
@@ -33,44 +33,41 @@ class Solution
             }
             return -1;
         }
-    int binearsearch(vector<int> nums,int target,int start, int end )
+    int binearsearch(vector<int> nums, int target, int start, int end)
     {
-        while(end>=start)
+        while (end >= start)
         {
-             int mid = (end+start)/2;
-             if( nums[mid]==target)
-             {
-                  return mid ;
-             }
-             if(nums[mid]>target)
-             {
-                 end= mid-1;
-             }
-            else {
-                start = mid+1;
+            int mid = (end + start) / 2;
+            if (nums[mid] == target)
+            {
+                return mid;
+            }
+            if (nums[mid] > target)
+            {
+                end = mid - 1;
+            }
+            else
+            {
+                start = mid + 1;
             }
         }
         return -1;
     }
-    int search(vector<int> &nums, int target) {
+    int search(vector<int> &nums, int target)
+    {
         int min = findMin(nums);
-        cout << min <<endl;
-        int need1=  binearsearch(nums,target,0,min-1);
-     int need2=   binearsearch(nums,target,min,nums.size()-1);
-         if( need1==-1 && need2!=-1)
-         {
-              return need2 ;
-         }
-        else if(need2==-1 && need1!=-1)
+
+        int need1 = binearsearch(nums, target, 0, min - 1);
+        int need2 = binearsearch(nums, target, min, nums.size() - 1);
+        if (need1 == -1 && need2 != -1)
         {
-             return need1;
+            return need2;
         }
-        
+        else if (need2 == -1 && need1 != -1)
+        {
+            return need1;
+        }
+
         return -1;
-        
-        
-        
-        
-        
     }
 };
