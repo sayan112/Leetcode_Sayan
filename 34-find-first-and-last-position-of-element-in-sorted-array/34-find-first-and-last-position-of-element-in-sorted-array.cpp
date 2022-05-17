6da5cdcf45387439b199d17656bb07c217281b8e
SAYAN MAITRA <75684139+sayan112@users.noreply.github.com>
2022-05-18 00:38:01 +0530

    Time: 13 ms (35.02%), Space: 13.6 MB (64.99%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,72 +1,32 @@
-class Solution
-{
-    public:
-    int  binarysearchfindstartX(vector<int> vec, int x)
-        {
-
-            int first = -1;
-            int start = 0;
-            int end = vec.size() - 1;
-            bool need = true;
-
-            while (start <= end)
-            {
-
-                int mid = start + (end - start) / 2;	//
-                if (vec[mid] == x)
-                {
-                    first = mid;
-                    end = mid - 1;
-                }
-                else if (vec[mid] < x)
-                {
-                    start = mid + 1;
-                }
-                else
-                {
-                    end = mid - 1;
-                }
-            }
-            return  first;
-        }
-
-    int binarysearchfindendX(vector<int> vec, int x)
-    {
-
-        int start = 0;
-        int finalop = -1;
-        int end = vec.size() - 1;
-        bool need = true;
-
-        while (start <= end)
-        {
-
-            int mid = start + (end - start) / 2;	//
-            if (vec[mid] == x)
-            {
-                finalop = mid;
-                start = mid + 1;
-            }
-            else if (vec[mid] < x)
-            {
-                start = mid + 1;
-            }
-            else
-            {
-                end = mid - 1;
-            }
-        }
-        return  finalop;
-    }
-
-    vector<int> searchRange(vector<int> &nums, int X)
-    {
-
-         vector<int>ans ; 
-        int op1= binarysearchfindstartX(nums, X);
-
-       int op2= binarysearchfindendX(nums, X);
-        ans.push_back(op1); ans.push_back(op2);
-        return ans;
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+        vector<int>ans ; 
+         int n = nums.size();
+         for(int i=0; i< nums.size();i++)
+         {
+             if(nums[i]==target)
+             {
+                 ans.push_back( i);
+                 break;
+             }
+         }
+         reverse(nums.begin(),nums.end());
+             for(int i=0; i< nums.size();i++)
+         {
+             if(nums[i]==target)
+             {
+                 ans.push_back( abs(i-n)-1);
+                 break;
+             }
+         }
+         if(ans.size()==0)
+         {
+              ans.push_back(-1);
+             ans.push_back(-1);
+             
+         }
+        
+         return ans ;
     }
 };
