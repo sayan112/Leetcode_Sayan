ce92b17b82696a3e8f40451dd0151b484f057fc7
SAYAN MAITRA <75684139+sayan112@users.noreply.github.com>
2022-05-19 00:00:40 +0530

    Time: 31 ms (66.61%), Space: 22.3 MB (56.96%) - LeetHub

--- 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.cpp
@@ -1,39 +1,44 @@
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
-        if(nums.size()==1)
-        {
-             return nums[0];
-        }
-    
-        int low =0;
-        int high = nums.size()-1;
-         if(nums[0]!=nums[1])
-         {
-              return nums[0];
-         }
-        else if (nums[high]!=nums[high-1] )
-        {
-             return nums[high];
-        }
         
-        while(low<=high)
-        {
-             int mid = (low+high)/2;
-            
-            if((nums[mid ]!= nums[mid-1]) && (nums[mid+1]!=nums[mid]))
+        
+         if(nums.size()==1)
+         {
+              return nums [0];
+         }
+        
+            if ( nums[0]!=nums[1])
             {
-                return nums [mid];
+                 return nums[0];
             }
-              if ( ((mid%2)==0 && (nums[mid]==nums[mid+1])) ||((mid%2!=0) && (nums[mid]==nums[mid-1]))  )
+         if (nums[nums.size()-1] != nums[nums.size()-2])
+         {
+              return nums[nums.size()-1];
+         }
+        
+         int  start =0;
+         int end = nums.size()-1;
+          while(end>=start)
+          {
+               int mid = (start+end)/2;
+             if( (nums[mid]!= nums[mid-1]) &&( nums[mid+1])!= nums[mid]  )
              {
-                 low= mid+1;
+                 return nums[mid];
              }
-            else{
-                high = mid-1;
-            }
-        }
-         return -1;
-    
+               if ( ( (mid%2==0) && ( nums[mid]== nums[mid+1] ))  || ( (mid%2!=0) && ( nums[mid]== nums[mid-1] )) )
+               {
+                   start = mid+1;
+               }
+              else{
+                   end = mid -1;
+              }
+              
+               
+          }
+         return 0;
+         
+        
+        
     }
 };
