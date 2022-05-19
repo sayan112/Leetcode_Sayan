b5dc28ebade528b262b2f6e8d4474335a71f9f98
SAYAN MAITRA <75684139+sayan112@users.noreply.github.com>
2022-05-19 11:41:30 +0530

    Time: 3 ms (53.90%), Space: 6 MB (99.15%) - LeetHub

---
+++ 28-implement-strstr/28-implement-strstr.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        
+      int need=  haystack.find(needle);
+         return need;
+    
+     
+        
+        
+    }
+};
