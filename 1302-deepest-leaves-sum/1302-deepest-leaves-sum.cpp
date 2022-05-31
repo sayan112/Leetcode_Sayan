c9b6a15257fff39f13d05148350d18e5e040e380
SAYAN MAITRA <75684139+sayan112@users.noreply.github.com>
2022-05-31 14:31:19 +0530

    Time: 95 ms (98.65%), Space: 59.7 MB (77.24%) - LeetHub

--- 1302-deepest-leaves-sum/1302-deepest-leaves-sum.cpp
+++ 1302-deepest-leaves-sum/1302-deepest-leaves-sum.cpp
@@ -12,39 +12,43 @@
 class Solution {
 public:
     int sum =0;
+    int maxdepth=0;
     
-     int  maxidpth(TreeNode *node)
-     {
-         if(!node)
-         {
-              return 0;
-         }
-          int left=1+maxidpth(node->left);
-           int right=1+maxidpth(node->right);
-          return max(left,right);
-     }
+//      int  maxidpth(TreeNode *node)
+//      {
+//          if(!node)
+//          {
+//               return 0;
+//          }
+//           int left=1+maxidpth(node->left);
+//            int right=1+maxidpth(node->right);
+//           return max(left,right);
+//      }
     
-    void findmaxdpthleaves(TreeNode *node,int cur ,int maxdepth)
+    void findmaxdpthleaves(TreeNode *node,int cur)
     {
-          if(!node)
+          if(node)
          {
-              return ;
+             if(cur>maxdepth)
+     {
+         sum=0;
+         maxdepth=cur;
+     }
+         if(cur==maxdepth)
+         {
+             sum+=node->val;
          }
-        if(node)
-        {
-            if(cur==maxdepth)
-            {
-                sum+=node->val;
-            }
-        }
-        findmaxdpthleaves(node->left,cur+1,maxdepth);
-         findmaxdpthleaves(node->right,cur+1,maxdepth);
-        
+        findmaxdpthleaves(node->left,cur+1);
+         findmaxdpthleaves(node->right,cur+1);
+         
+         }
+       
+    
     }
     
     int deepestLeavesSum(TreeNode* root) {
-        int maxdepth= maxidpth(root);
-     findmaxdpthleaves(root,1,maxdepth);
+        // int maxdepth= maxidpth(root);
+     findmaxdpthleaves(root,1);
     
         return sum;
     }
