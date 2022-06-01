f4e1934f29b0ac7593d38b463288ceedbd4166c6
SAYAN MAITRA <75684139+sayan112@users.noreply.github.com>
2022-06-01 23:54:42 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (80.67%) - LeetHub

--- 50-powx-n/50-powx-n.cpp
+++ 50-powx-n/50-powx-n.cpp
@@ -1,22 +1,37 @@
-class Solution {
-public:
-    double myPow(double x, int n) {
-       
-        if(n==0){
+class Solution
+{
+    public:
+        double ans(double x, long long int p)
+        {
+              if(p==0){
             return 1;
         }
         if(x==1){
             return 1;
         }
-        if(n<0){
+            
+            if (p % 2 == 0)
+            {
+                return ans(x*x, p / 2);
+            }
+            else
+            {
+                return x*ans(x*x, p / 2);
+            }
+        }
+
+    double myPow(double x, long long int n)
+    {
+
+        if (n < 0)
+        {
+
             n = abs(n);
-            x = 1/x;
+            x = 1 / x;
         }
-        if(n%2==0){
-            return myPow(x*x,n/2);
-        }
-        else{
-            return x*myPow(x*x,n/2);
-        }
+
+        return ans(x, n);
+
+        
     }
 };
