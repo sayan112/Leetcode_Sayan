50ce162835a276aeaf8b02ddea62b2bac690f60c
SAYAN MAITRA <75684139+sayan112@users.noreply.github.com>
2022-06-02 16:28:40 +0530

    Time: 11 ms (88.86%), Space: 10.7 MB (43.03%) - LeetHub

--- 867-transpose-matrix/867-transpose-matrix.cpp
+++ 867-transpose-matrix/867-transpose-matrix.cpp
@@ -1,13 +1,18 @@
-class Solution {
-public:
- vector<vector<int>> transpose(vector<vector<int>>& matrix) {
-        int m=matrix.size(), n=matrix[0].size();
-        vector<vector<int>> trans (n, vector<int>(m,0));
-		
-        for(int i=0; i<m; ++i)
-            for(int j=0; j<n; ++j)
-                trans[j][i] = matrix[i][j];
-				
-        return trans;
-    }
+class Solution
+{
+    public:
+        vector<vector < int>> transpose(vector<vector < int>> &matrix)
+        {
+            int n = matrix.size();
+            int m = matrix[0].size();
+            vector<vector < int>> ans(m, vector<int> (n, 0));
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    ans[j][i] = matrix[i][j];
+                }
+            }
+            return ans;
+        }
 };
