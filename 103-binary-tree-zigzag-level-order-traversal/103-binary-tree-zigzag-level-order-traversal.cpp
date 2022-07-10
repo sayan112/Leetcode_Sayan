183eb2e6daa1e2e53e8e3069a98e7c50e029dd39
SAYAN MAITRA <75684139+sayan112@users.noreply.github.com>
2022-07-10 21:39:08 +0530

    Time: 7 ms (40.40%), Space: 12.2 MB (16.05%) - LeetHub

--- 103-binary-tree-zigzag-level-order-traversal/103-binary-tree-zigzag-level-order-traversal.cpp
+++ 103-binary-tree-zigzag-level-order-traversal/103-binary-tree-zigzag-level-order-traversal.cpp
@@ -1,59 +1,66 @@
 /**
  *Definition for a binary tree node.
  *struct TreeNode {
- *    int val;
- *    TreeNode * left;
- *    TreeNode * right;
- *    TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *   int val;
+ *   TreeNode * left;
+ *   TreeNode * right;
+ *   TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *   TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *   TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  *};
  */
+
+
 class Solution
 {
     public:
         vector<vector < int>> zigzagLevelOrder(TreeNode *root)
         {
-                        vector<vector < int>> ans;
-            queue<TreeNode*> q;
-             if(root==NULL)
-             {
-                  return ans;
-             }
-            q.push(root);
-            int flag = 0;
-
-            while (!q.empty())
+             vector<vector < int>> ans ; 
+            if(!root)
             {
-                int size = q.size();
-vector<int>vec;
-                for (int i = 0; i < size; i++)
-                {
-                    TreeNode *node = q.front();
-                    q.pop();
-
-                    if (node->left)
-                    {
-                        q.push(node->left);
-                    }
-                    if (node->right)
-                    {
-                        q.push(node->right);
-                    }
-                    vec.push_back(node->val);
-                }
-                if (flag == 0)
-                {
-                    flag = 1;
-                    ans.push_back(vec);
-                }
-                else
-                {
-                    flag = 0;
-                    reverse(vec.begin(), vec.end());
-                    ans.push_back(vec);
-                }
+                return ans;
             }
-            return ans;
+      
+    queue<TreeNode *>q;
+    q.push(root);
+              int flag =0;
+     while(q.size())
+     {
+         int size=q.size();
+         vector<int>need ;
+        
+         
+         for(int i =0; i < size;i++)
+         {
+             
+              TreeNode *node=q.front();
+          q.pop();
+             if(node->left)
+             {
+                 q.push(node->left);
+             }
+             if(node->right)
+             {
+                 q.push(node->right);
+             }
+             need.push_back(node->val);
+         }
+          if(flag==0)
+          {
+              ans.push_back(need);
+              flag=1;
+          }
+         else{
+             reverse(need.begin(),need.end());
+             flag=0;
+             ans.push_back(need);
+         }
+         
+          
+         
+        
+     }
+             return ans;
         }
 };
