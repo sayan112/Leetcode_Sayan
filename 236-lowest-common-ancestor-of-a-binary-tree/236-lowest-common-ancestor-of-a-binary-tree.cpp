b448cf8a8600caee28d09a66e962c60c12f83cb3
SAYAN MAITRA <75684139+sayan112@users.noreply.github.com>
2022-07-26 21:52:47 +0530

    Time: 22 ms (64.82%), Space: 17.4 MB (9.00%) - LeetHub

---
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -0,0 +1,99 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+
+    bool findpath (TreeNode* root, TreeNode* need, vector<TreeNode*>&vec)
+    {
+        
+         if(!root)
+         {
+              return false;
+         }
+        
+        vec.push_back(root);
+         if(root==need)
+         {
+              return true;
+         }
+         if(findpath(root->left,need,vec) || findpath(root->right,need,vec) )
+         {
+             return true;
+         }
+         vec.pop_back();
+         return false;
+        
+        
+    }
+    public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+         vector<TreeNode*>vec1;
+     
+           vector<TreeNode*>vec2;
+        findpath(root, p ,vec1);
+        findpath(root, q , vec2 );
+       
+         TreeNode *lastCommon=NULL;
+        for(int i =0; i <  min (vec1.size(),vec2.size());i++)
+        {
+            if(vec1[i]==vec2[i])
+            {
+                lastCommon=vec1[i];
+            }
+          
+        }
+        
+        
+      
+         return lastCommon ;
+        
+    }
+};
+
+
+
+
+
+
+// // Nikhil Nagrale
+// // 10 months ago
+// // // Code For Naive Solution 
+// class Solution {
+//     bool getPath(TreeNode* root, TreeNode* x, vector<TreeNode*> &path) {
+//         if (root == NULL) return false;
+//         path.push_back(root);
+//         if(root == x) return true;
+//         if (getPath(root->left, x, path) || getPath(root->right, x, path))
+//             return true;
+//         path.pop_back();
+//         return false;
+//     }
+
+//     vector<TreeNode > getPathtoNode(TreeNode A, TreeNode* B) {
+//         vector<TreeNode *> path;
+//         if (A == NULL) return path;
+//         getPath(A, B, path);
+//         return path;
+//     }
+    
+//     TreeNode* findLastCommon(vector<TreeNode*> a,vector<TreeNode*> b){
+//         TreeNode *lastCommon=NULL;
+//         for(int i=0;i<min(a.size(),b.size());i++){
+//             if(a[i]==b[i]) lastCommon=a[i];
+//         }
+//         return lastCommon;
+//     }
+// public:
+//     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+//         vector<TreeNode*> pathToP = getPathtoNode(root,p);
+//         vector<TreeNode*> pathToQ = getPathtoNode(root,q);
+//         TreeNode *LCA = findLastCommon(pathToP,pathToQ);
+//         return LCA;
+//     }
+// };
