b4591e04b95a888837f1e9686ef7cd0bc91e699a
SAYAN MAITRA <75684139+sayan112@users.noreply.github.com>
2022-07-31 13:28:47 +0530

    Time: 50 ms (56.99%), Space: 30.8 MB (63.77%) - LeetHub

--- 222-count-complete-tree-nodes/222-count-complete-tree-nodes.cpp
+++ 222-count-complete-tree-nodes/222-count-complete-tree-nodes.cpp
@@ -1,39 +1,72 @@
 /**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
+ *Definition for a binary tree node.
+ *struct TreeNode {
+ *    int val;
+ *    TreeNode * left;
+ *    TreeNode * right;
+ *    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *};
  */
-class Solution {
-public:
-     void countallnode(TreeNode* root , int &count )
+class Solution
+{
+    public:
+    
+    // approach count height 
+    // if left height and right height is equal then count height and from there we can count nodes 
+    // else 1+ (count-leftsub-tree)+(count-rightsub-tree)
+     int heightleft(TreeNode *root)
      {
-          if(!root)
-          {
-              return ; 
-          }
-              
-         count++;
-         countallnode(root->left, count);
-         countallnode(root->right,count);
-         
+          int count =0;
+         while(root)
+         {
+             root=root->left;
+             count++;
+         }
+          return count ; 
          
      }
-    int countNodes(TreeNode* root) {
-        
-        if(!root)
+     int heightright(TreeNode *root)
+     {
+          int count =0;
+         while(root)
+         {
+             root=root->right;
+             count++;
+         }
+          return count ; 
+         
+     }
+        int  countallnode(TreeNode *root)
         {
-             return 0;
+             if(!root)
+             {
+                  return 0;
+             }
+             int leftheight= heightleft(root) ;
+            int rightheight = heightright(root);
+             if( leftheight==rightheight )
+             {
+                 return(1<<leftheight)-1;
+             }
+            else {
+                 return 1+ (countallnode(root->left))+(countallnode(root->right));
+                }
+            
         }
-        else{
-            int count =0;
-            countallnode(root,count);
-            return count ;
+    int countNodes(TreeNode *root)
+    {
+
+        if (!root)
+        {
+            return 0;
+        }
+        else
+        {
+        
+     return countallnode(root);
+    
         }
     }
 };
