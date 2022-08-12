0481d32cb7246963b38ccf3afd426e61d9c76389
SAYAN MAITRA <75684139+sayan112@users.noreply.github.com>
2022-08-13 00:15:10 +0530

    Time: 1216 ms (5.08%), Space: 183 MB (7.45%) - LeetHub

---
+++ 1609-even-odd-tree/1609-even-odd-tree.cpp
@@ -0,0 +1,104 @@
+/**
+ *Definition for a binary tree node.
+ *struct TreeNode {
+ *   int val;
+ *   TreeNode * left;
+ *   TreeNode * right;
+ *   TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *   TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *   TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *};
+ */
+class Solution
+{
+    public:
+
+        bool isEvenOddTree(TreeNode *root)
+        {
+            vector<vector < int>> ans;
+
+            queue<TreeNode*> q;
+            q.push(root);
+
+            while (!q.empty())
+            {
+                vector<int> vec;
+                int size = q.size();
+                for (int i = 0; i < size; i++)
+                {
+                    TreeNode *node = q.front();
+                    q.pop();
+                    vec.push_back(node->val);
+                    if (node->left)
+                    {
+                        q.push(node->left);
+                    }
+                    if (node->right)
+                    {
+                        q.push(node->right);
+                    }
+                }
+                ans.push_back(vec);
+            }
+
+            int check = 0;
+             for(auto elem1: ans )
+             {
+                  for(auto elem : elem1)
+                  {
+                      cout << elem <<" ";
+                  }
+                 cout <<endl;
+             }
+            for (auto elem1: ans)
+            {
+ 
+                if (check == 0)
+                {
+                    check = 1;
+                    for (auto elem: elem1)
+                    {
+                        if (elem % 2 == 0)
+                        {
+                            return false;
+                        }
+                    }
+                    for (int i = 1; i < elem1.size(); i++)
+                    {
+                        if (elem1[i] <= elem1[i - 1])
+                        {
+                            return false;
+                        }
+                    }
+                }
+                else
+                {
+                     for(auto elem : elem1)
+                     {
+                         cout << elem <<" ";
+                     }
+                    cout <<endl;
+
+                    check = 0;
+                    for (auto elem: elem1)
+                    {
+                        if (elem % 2 != 0)
+                        {                             cout << "*1"<<endl;
+                            return false;
+                        }
+                    }
+                    for (int i = 1; i < elem1.size(); i++)
+                    {
+                        if (elem1[i] >= elem1[i - 1])
+                        {
+
+                            return false;
+                        }
+                    }
+                }
+            }
+              return true;
+        }
+  
+
+};
