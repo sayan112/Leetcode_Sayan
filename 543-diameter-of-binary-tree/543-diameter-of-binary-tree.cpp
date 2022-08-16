95d6f611dff2419e00a5ef2511626e467f5c35be
SAYAN MAITRA <75684139+sayan112@users.noreply.github.com>
2022-08-17 00:56:01 +0530

    Time: 24 ms (26.58%), Space: 20.2 MB (65.78%) - LeetHub

--- 543-diameter-of-binary-tree/543-diameter-of-binary-tree.cpp
+++ 543-diameter-of-binary-tree/543-diameter-of-binary-tree.cpp
@@ -1,41 +1,42 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
-class Solution {
-public:
-     int maxi=0;
-    int    countdiameter(TreeNode* node)
-    {
-         if(!node)
+
+class Solution
+{
+    public:
+        int maxi = 0;
+     int countheight(TreeNode *node)
+     {
+          if(!node)
          {
               return 0 ;
          }
-      int leftheight=   countdiameter(node->left);
-       int rightheight=   countdiameter(node->right);
-        maxi=max( maxi , (leftheight+rightheight) );
+      int left=  countheight(node->left);
+       int right=  countheight(node->right);
+        return 1+max(left,right);
+     }
+void countdiameter(TreeNode *node)
+    {
+        if (!node)
+        {
+            return ;
+        }
+        int leftheight = countheight(node->left);
+        int rightheight = countheight(node->right);
+       int sum =  leftheight +  rightheight;
+        maxi=max(maxi,sum);
+        countdiameter(node->left);
+        countdiameter(node->right);
         
-        return 1+max(leftheight, rightheight);
-         
-    }
-     
-    int diameterOfBinaryTree(TreeNode* root) {
-        
-         if(!root)
-         {
-              return 0;
-         }
-        
-        
-      countdiameter(root);
-         return maxi;
-        
+
+    } 
+
+    int diameterOfBinaryTree(TreeNode *root)
+    {
+        if (!root)
+        {
+            return 0;
+        }
+
+        countdiameter(root);
+        return maxi;
     }
 };
