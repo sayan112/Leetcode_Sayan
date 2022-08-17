2341769c002d9d29682698e10a94f8bf457fb1fa
SAYAN MAITRA <75684139+sayan112@users.noreply.github.com>
2022-08-17 20:22:06 +0530

    Time: 0 ms (100.00%), Space: 12.3 MB (7.59%) - LeetHub

--- 103-binary-tree-zigzag-level-order-traversal/103-binary-tree-zigzag-level-order-traversal.cpp
+++ 103-binary-tree-zigzag-level-order-traversal/103-binary-tree-zigzag-level-order-traversal.cpp
@@ -12,47 +12,49 @@
 class Solution {
 public:
     vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
-     
-         queue<TreeNode*>q;
-         q.push(root);
-         vector<vector<int>>ans ; 
-        if(!root)
-        {
-             return ans ;
-        }
-        int check =0;
-         
+        queue<TreeNode*>q;
+       
+        q.push(root);
+         vector<vector<int>>vec;
+          if(!root)
+         {
+          return vec;   
+         }
+         int check =0;
          while(!q.empty())
          {
-              vector<int>vec;
-              int size = q.size();
-              for(int i =0; i < size;i++)
+              vector<int>need;
+             
+             int size=q.size();
+              while(size--)
               {
-                  TreeNode*node = q.front();
-                   q.pop();
-                   vec.push_back(node->val);
-                    if(node->left)
-                  {
-                      q.push(node->left);
-                  }
-                  if(node->right)
-                  {
-                      q.push(node->right);
-                  } 
-                 
+                   
+                 TreeNode*node=q.front();
+                  q.pop();
+                   if(node->left)
+                   {
+                       q.push(node->left);
+                   }
+                      if(node->right)
+                   {
+                       q.push(node->right);
+                   }
+                  need.push_back(node->val);
+                  
               }
               if(check==0)
               {
-                   ans.push_back(vec);
+                     vec.push_back(need);
                   check=1;
-                  
-              }else{
-                  reverse(vec.begin(),vec.end());
-                   ans.push_back(vec);
-                  check=0;
               }
+             else{
+                 reverse(need.begin(),need.end());
+                 vec.push_back(need);
+                 check=0;
+             }
+          
              
          }
-         return ans ; 
+        return vec;
     }
 };
