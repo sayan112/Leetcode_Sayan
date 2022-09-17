e65473b2be107a1671569dab9aecb9b6da911353
SAYAN MAITRA <75684139+sayan112@users.noreply.github.com>
2022-09-17 15:21:51 +0530

    Time: 13 ms (13.60%), Space: 10.8 MB (32.53%) - LeetHub

--- 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.cpp
+++ 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.cpp
@@ -1,39 +1,46 @@
 /**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
+ *Definition for singly-linked list.
+ *struct ListNode {
+ *   int val;
+ *   ListNode * next;
+ *   ListNode() : val(0), next(nullptr) {}
+ *   ListNode(int x) : val(x), next(nullptr) {}
+ *   ListNode(int x, ListNode *next) : val(x), next(next) {}
+ *};
  */
-class Solution {
-public:
-    ListNode* removeNthFromEnd(ListNode* head, int n) {
-      
-         vector<int>vec;
-         while(head)
-         {
-      
-              vec.push_back(head->val);
-                    head=head->next;
-              
-         }
-         ListNode* ans= new ListNode();
-        ListNode* node=ans;
-        for(int i =0; i < vec.size();i++)
+class Solution
+{
+    public:
+        ListNode* removeNthFromEnd(ListNode *head, int n)
         {
-            if(i==(vec.size()-n))
+
+            vector<int> vec; 
+            
+            // putting all node values into a vector :)
+            
+            while (head)
             {
-                 continue;
+                vec.push_back(head->val);
+                head = head->next;
             }
-            ListNode* helper = new ListNode(vec[i]);
-             node->next = helper;
-             node= node->next;
+            
+            
+            // now create new node and add values into it without the nth value from end
+            ListNode *ans = new ListNode();
+            ListNode *node = ans;
+            for (int i = 0; i < vec.size(); i++)
+            {
+                if (i == (vec.size() - n))
+                {
+                    continue;
+                }
+                ListNode *helper = new ListNode(vec[i]);
+                node->next = helper;
+                node = node->next;
+            }
+            
+            // now return just ans->next :)
+            
+            return ans->next;
         }
-         return ans->next;
-        
-        
-    }
 };
