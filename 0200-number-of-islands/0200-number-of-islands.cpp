ce2ad82f7dba5311b4b51c5cbe360ad77a08a323
SAYAN MAITRA <75684139+sayan112@users.noreply.github.com>
2022-10-16 20:11:01 +0530

    Time: 110 ms (9.51%), Space: 13.4 MB (25.18%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,78 +1,49 @@
 class Solution
 {
     public:
-        void BFS(vector<vector < char>> &grid, int row, int col, vector< vector< int>> &visited , int del_row[], int del_col[] )
+        void dfs(int row, int col, vector<vector < int>> &visited, vector< vector< char>> &grid, int n, int m, int delrow[], int delcol[])
         {
-            visited[row][col] = 1;
-            int n = grid.size();
-            int m = grid[0].size();
-            queue<pair<int, int>> q;
-            q.push({ row,
-                col });
-            while (!q.empty())
-            {
-                int row = q.front().first;
-                int col = q.front().second;
-                q.pop();
-                
-               	//checking all  8 direction 
+            if (row < 0 || col < 0 || row > grid.size() - 1 || col > grid[0].size() - 1 || visited[row][col] || grid[row][col] == '0') return;
 
-//                 for (int delcol = -1; delcol <= 1; delcol++)
-//                 {
-//                     for (int delrow = -1; delrow <= 1; delrow++)
-//                     {
-//                         int nrow = delrow + row;
-//                         int ncol = delcol + col;
-//                         if (nrow >= 0 && nrow < n && ncol >= 0 && ncol < m && grid[nrow][ncol] == '1' && visited[nrow][ncol] == 0)
-//                         {
-//                             visited[nrow][ncol] = 1;
-//                             q.push({ nrow,
-//                                 ncol });
-//                         }
-//                     }
-//                 }
-                
-                
-                // we have to check only 4 direction 
-                 for (int i =0; i < 4 ;i++ )
-                 {
-                    
-                             int nrow = del_row[i] + row;
-                        int ncol = del_col[i] + col;
-                        if (nrow >= 0 && nrow < n && ncol >= 0 && ncol < m && grid[nrow][ncol] == '1' && visited[nrow][ncol] == 0)
-                        {
-                            visited[nrow][ncol] = 1;
-                            q.push({ nrow,
-                                ncol });
-                        }
-                 }
+            for (int i = 0; i < 4; i++)
+            {
+                int nrow = row + delrow[i];
+                int ncol = col + delcol[i];
+
+                visited[row][col] = 1;
+                dfs(nrow, ncol, visited, grid, n, m, delrow, delcol);
             }
         }
-
     int numIslands(vector<vector < char>> &grid)
     {
 
         int n = grid.size();
         int m = grid[0].size();
         vector<vector < int>> visited(n, vector<int> (m, 0));
-        int count = 0;
-        // for four directions 
-        int del_row[] = {-1, 0, 1, 0};
-        int del_col[] = {0, 1, 0, -1};
+        int delrow[] = { -1,
+            0,
+            1,
+            0
+        };
+        int delcol[] = { 0,
+            1,
+            0,
+            -1
+        };
+        int countofisland = 0;
         for (int i = 0; i < n; i++)
         {
             for (int j = 0; j < m; j++)
             {
-
-                if (visited[i][j]==0 && grid[i][j] == '1')
+                if (grid[i][j] == '1' && visited[i][j] == 0)
                 {
-                    int row = i;
-                    int col = j;
-                    count++;
-                    BFS(grid, row, col, visited ,del_row, del_col);
+
+                    dfs(i, j, visited, grid, n, m, delrow, delcol);
+
+                    countofisland++;
                 }
             }
         }
-        return count;
+        return countofisland;
     }
 };
