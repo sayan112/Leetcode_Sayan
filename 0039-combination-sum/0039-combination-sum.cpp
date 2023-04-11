5a5bc1860308db9a0355e3f5b88b5b464da80897
SAYAN MAITRA <75684139+sayan112@users.noreply.github.com>
2023-04-11 20:44:12 +0530

    Time: 6 ms (70.47%), Space: 10.8 MB (82.45%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,39 +1,35 @@
-class Solution
+class Solution {
+public:
+    
+void generateallsolutions (vector<int>& candidates , int target, int idx ,vector<int>&ds, vector<vector<int>>&ans)
+    
 {
-    public:
-
-        void need(vector<int> &candidates, int target, int idx, vector<int> &ansvec, vector< vector< int>> &ans) {
-            if(idx==candidates.size())
-            {
-                if(target==0)
-                {
-                     ans.push_back(ansvec);
-              
-                }     
-                return ; 
-            }
-            
-            if(target>=candidates[idx])
-            {
-               ansvec.push_back(candidates[idx]);
-                
-                  need(candidates, target-candidates[idx], idx, ansvec,ans);
-                    ansvec.pop_back();
-            }
-         
-           
-            
-            need(candidates, target, idx+1, ansvec, ans);
-            
-            
-        }
-    vector<vector < int>> combinationSum(vector<int> &candidates, int target)
+ 
+    if(idx==candidates.size())
     {
-
-        vector<int> ansvec;
-        vector<vector < int>> ans;
-        int idx = 0;
-        need(candidates, target, idx,  ansvec, ans);
+        if(target==0)
+        {
+             ans.push_back(ds);
+         
+        }
+            return;
+    }
+    
+    if(target>=candidates[idx])
+    {
+        ds.push_back(candidates[idx]);
+        generateallsolutions(candidates,target-candidates[idx],idx,ds,ans);
+        ds.pop_back();
+    }
+        generateallsolutions(candidates,target,idx+1,ds,ans);
+    
+    
+}
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        int idx=-0;
+         vector<int>ds;
+    vector<vector<int>> ans;
+         generateallsolutions(candidates,target,idx,ds,ans);
          return ans;
     }
 };
