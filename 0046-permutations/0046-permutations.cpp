5085d1e8de66c828878dee12f7f057dee5844626
SAYAN MAITRA <75684139+sayan112@users.noreply.github.com>
2023-04-11 13:06:16 +0530

    Time: 3 ms (71.96%), Space: 7.7 MB (79.82%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,24 +1,33 @@
-class Solution {
-public:
-     void generateallPermutations (vector<int>& nums , int idx ,  vector<vector<int>>&ds )
-     {
-         
-          if(idx>=nums.size())
-          {
-               ds.push_back(nums);
-               return ;
-          }
-          for(int i=idx; i < nums.size();i++)
-          {
-               swap(nums[idx],nums[i]);
-              generateallPermutations(nums,idx+1,ds);
-                swap(nums[idx],nums[i]);
-          }
-     }
-    vector<vector<int>> permute(vector<int>& nums) {
-          vector<vector<int>>ds;
-         int idx =0;
-         generateallPermutations(nums,idx,ds);
-         return ds;
+class Solution
+{
+    public:
+        void generateallpermutation(vector<int> &nums, int idx, vector<vector< int >> &ans, vector< bool > &ds, vector< int > &ansvec)
+        {
+            if (ansvec.size() == nums.size())
+            {
+                ans.push_back(ansvec);
+                return;
+            }
+            for (int i = 0; i < nums.size(); i++)
+            {
+                if (ds[i])
+                {
+                     ds[i]=false;
+                    ansvec.push_back(nums[i]);
+               
+                    generateallpermutation(nums, idx + 1, ans, ds, ansvec);
+                    ansvec.pop_back();
+                    ds[i] = true;
+                }
+            }
+        }
+    vector<vector < int>> permute(vector<int> &nums)
+    {
+        int idx = 0;
+        vector<vector < int>> ans;
+        vector<bool> ds(nums.size(), true);
+        vector<int> ansvec;
+        generateallpermutation(nums, idx, ans, ds, ansvec);
+        return ans;
     }
 };
