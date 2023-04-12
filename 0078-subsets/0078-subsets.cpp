0c93eb5ce3b5109f352a28ac0ebc1b540ee99206
SAYAN MAITRA <75684139+sayan112@users.noreply.github.com>
2023-04-12 23:16:31 +0530

    Time: 5 ms (36.98%), Space: 7.1 MB (89.09%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,24 +1,22 @@
-class Solution
-{
-    public:
-        void generateallsubsets(vector<int> &nums, int idx, vector<vector< int >> &ans, vector< int > &ds)
-        {
-            if (nums.size() <= idx)
-            {
-                ans.push_back(ds);
-                return;
-            }
-            ds.push_back(nums[idx]);
-            generateallsubsets(nums, idx + 1, ans, ds);
-            ds.pop_back();
-            generateallsubsets(nums, idx + 1, ans, ds);
-        }
-    vector<vector < int>> subsets(vector<int> &nums)
-    {
-        int idx = 0;
-        vector<vector < int>> ans;
-        vector<int> ds;
-        generateallsubsets(nums, idx, ans, ds);
-         return ans;
+class Solution {
+public:
+     void storeallsubsets(vector<int>& nums, int idx , vector<int>&ds, vector<vector<int>>&ans)
+     {
+          if(idx==nums.size())
+          {
+               ans.push_back(ds);
+               return ;
+          }
+          ds.push_back(nums[idx]);
+                 storeallsubsets(nums,idx+1,ds,ans);
+         ds.pop_back();
+                 storeallsubsets(nums,idx+1,ds,ans);
+     }
+    vector<vector<int>> subsets(vector<int>& nums) {
+        int idx =0;
+             vector<vector<int>>ans;
+         vector<int>ds;
+        storeallsubsets(nums,idx,ds,ans);
+         return ans ;
     }
 };
