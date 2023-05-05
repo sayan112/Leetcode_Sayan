a9c18ff497681dfe7cbd8ca8cf4f6e92621375d7
SAYAN MAITRA <75684139+sayan112@users.noreply.github.com>
2023-05-05 21:32:54 +0530

    Time: 208 ms (5.62%), Space: 9.7 MB (96.80%) - LeetHub

--- 1026-maximum-difference-between-node-and-ancestor/1026-maximum-difference-between-node-and-ancestor.cpp
+++ 1026-maximum-difference-between-node-and-ancestor/1026-maximum-difference-between-node-and-ancestor.cpp
@@ -1,36 +1,47 @@
 /**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
+ *Definition for a binary tree node.
+ *struct TreeNode {
+ *    int val;
+ *    TreeNode * left;
+ *    TreeNode * right;
+ *    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *};
  */
-class Solution {
-public:
-      int maxidiff =0;
-      void findmaxi (TreeNode* root , int child , int &maxidiff)
-      {
-          if(!root)
-          {
-               return ;
-          }
-            maxidiff=max(maxidiff,abs((root->val)-child));
-          findmaxi(root->left,child,maxidiff);
-          findmaxi(root->right,child,maxidiff);
-      }
-    int maxAncestorDiff(TreeNode* root) {
-        if(!root)
+class Solution
+{
+    public:
+
+        void findmaxi(TreeNode *root, int child, int &maxidiff)
         {
-             return 0;
+            if (!root)
+            {
+                return;
+            }
+            maxidiff = max(maxidiff, abs((root->val) - child));
+            findmaxi(root->left, child, maxidiff);
+            findmaxi(root->right, child, maxidiff);
         }
-       
-         findmaxi(root,root->val,maxidiff);
-         maxAncestorDiff(root->left);
-         maxAncestorDiff(root->right);
-         return maxidiff;
+    void findmaxAncestorDiff(TreeNode *root, int &maxidiff)
+    {
+        if (!root)
+        {
+            return;
+        }
+        findmaxi(root, root->val, maxidiff);
+        findmaxAncestorDiff(root->left,maxidiff);
+        findmaxAncestorDiff(root->right,maxidiff);
+    }
+    int maxAncestorDiff(TreeNode *root)
+    {
+        if (!root)
+        {
+            return 0;
+        }
+        int maxidiff = 0;
+        findmaxAncestorDiff(root, maxidiff);
+
+        return maxidiff;
     }
 };
