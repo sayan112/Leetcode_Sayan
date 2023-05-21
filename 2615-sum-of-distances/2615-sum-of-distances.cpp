5738b796c9d3a1a431dd4c1d591505d3ded4c615
SAYAN MAITRA <75684139+sayan112@users.noreply.github.com>
2023-05-21 22:45:02 +0530

    Time: 328 ms (60.85%), Space: 116.4 MB (54.13%) - LeetHub

---
+++ 2615-sum-of-distances/2615-sum-of-distances.cpp
@@ -0,0 +1,28 @@
+class Solution
+{
+    public:
+        vector < long long > distance(vector<int> &arr)
+        {
+           int n = arr.size();
+        vector<long long> ans(n);
+        unordered_map<int, vector<int>> mp;
+        for(int i = 0; i<n; i++){
+            mp[arr[i]].push_back(i);
+        }
+        for(auto itr : mp){
+            vector<int> v = itr.second;
+            int size = v.size();
+            long long pre[size+1];
+            pre[0] = 0;
+            for(int i = 0; i<size; i++){
+                pre[i+1] = (long long)pre[i] + (long long)v[i];
+            
+            }
+            
+            for(long long i = 0; i<size; i++){
+                ans[v[i]] = i*v[i] - pre[i] + pre[size] - pre[i+1] - (size-i-1)*v[i];
+            }
+        }
+        return ans;
+        }
+};
