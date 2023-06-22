0cc18ce12dc853ae7873f7538650440a5503723d
SAYAN MAITRA <75684139+sayan112@users.noreply.github.com>
2023-06-22 18:06:16 +0530

    Time: 19 ms (79.17%), Space: 19.6 MB (15.05%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,23 +1,26 @@
-class Solution {
-public:
-    int majorityElement(vector<int>& nums) {
-       int ans =nums[0];
-         int count =1;
-         for(int i=1;i<nums.size();i++)
-         {
-                  if(count ==0)
-                  {
-                      ans=nums[i];
-                  }
-             if(nums[i]==ans)
-             {
-                 count++;
-             }
-             else{
-                 count--;
-             
-             }
-         }
-         return ans;
-    }
+class Solution
+{
+    public:
+        int majorityElement(vector<int> &nums)
+        {
+            int ans = nums[0];
+            int count = 1;
+            for (int i = 1; i < nums.size(); i++)
+            {
+                // moor voting algo
+                if (count == 0)
+                {
+                    ans = nums[i];
+                }
+                if (nums[i] == ans)
+                {
+                    count++;
+                }
+                else
+                {
+                    count--;
+                }
+            }
+            return ans;
+        }
 };
