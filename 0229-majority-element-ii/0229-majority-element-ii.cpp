072060ea6bb259b38ae4343d5fc5a447819d7845
SAYAN MAITRA <75684139+sayan112@users.noreply.github.com>
2023-06-22 22:26:52 +0530

    Time: 15 ms (64.96%), Space: 15.8 MB (94.46%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,20 +1,62 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-     unordered_map<int,int>mp;
-         for(auto elem : nums)
-         {
-              mp[elem]++;
-             
-         }
-         vector<int>ans;
-        for(auto elem : mp)
+   // we can apply boyre moore voting algo 
+         int count1=0;
+         int count2=0;
+        int elem1=INT_MIN;
+         int elem2=INT_MIN;
+        for(auto elem : nums)
         {
-             if(elem.second>nums.size()/3)
-             {
-                 ans.push_back(elem.first);
-             }
+            if(count1==0 && elem2!=elem)
+            {
+                count1++;
+                elem1=elem;
+            }
+            else if(count2==0 && elem1!=elem)
+            {
+                count2++;
+                elem2=elem;
+            }
+            else if(elem1==elem)
+            {
+                count1++;
+            }
+            else if(elem2==elem)
+            {
+                count2++;
+            }
+            else{
+                count1--;
+                count2--;
+            }
+            
         }
-         return ans ;
+        vector<int>ans;
+     count1 =0;
+         count2=0;
+         for(auto elem :nums)
+         {
+             if(elem==elem1)
+             {
+                 count1++;
+             }
+             else if(elem==elem2)
+             {
+                 count2++;
+             }
+         }
+        if(count1>nums.size()/3)
+        {
+            ans.push_back(elem1);
+        }
+          if(count2>nums.size()/3)
+        {
+            ans.push_back(elem2);
+        }
+        return ans;
+      
+        
+        
     }
 };
