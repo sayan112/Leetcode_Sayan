0128c19b6553d3e09514e7fdb44feef233103ef7
SAYAN MAITRA <75684139+sayan112@users.noreply.github.com>
2023-06-30 17:57:14 +0530

    Time: 282 ms (96.72%), Space: 134.5 MB (90.76%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -4,16 +4,13 @@ public:
   
 {
          int n = arr.size();
-        deque<int> dq; // Double-ended queues are sequence containers with the feature of expansion and contraction on both ends. They are similar to vectors, but are more efficient in case of insertion and deletion of elements. Unlike vectors, contiguous storage allocation may not be guaranteed. 
+        deque<int> dq;
         vector<int> res;
         int i = 0, j = 0;
         while(j < n)
         {
             while(!dq.empty() && dq.back() < arr[j])
-            {
-                       dq.pop_back();
-            }
-         
+                dq.pop_back();
                 
             dq.push_back(arr[j]);
              if(j - i + 1< k)
