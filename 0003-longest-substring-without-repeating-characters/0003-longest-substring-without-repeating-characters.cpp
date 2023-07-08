844fe3402214d3c93b1fe88bdf9d20f7bc6eabb4
SAYAN MAITRA <75684139+sayan112@users.noreply.github.com>
2023-07-08 18:10:23 +0530

    Time: 23 ms (56.32%), Space: 8.9 MB (42.00%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,39 +1,38 @@
-class Solution
-{
-    public:
-        int lengthOfLongestSubstring(string s)
-        {
-
-            int i = 0;
-            int j = 0;
-            unordered_map<char, int> mp;
-            int maxi = 0;
-            while (j < s.size())
-            {
-                mp[s[j]]++;
-                if (mp.size() > (j - i + 1))
-                {
-                    j++;
-                }
-                else if (mp.size() == (j - i + 1))
-                {
-                    maxi = max(maxi, j - i + 1);
-                    j++;
-                }
-                else
-                {
-                    while (mp.size() < (j - i + 1))
-                    {
-                        mp[s[i]]--;
-                        if (mp[s[i]] == 0)
-                        {
-                            mp.erase(s[i]);
-                        }
-                        i++;
-                    }
-                    j++;
-                }
-            }
-             return maxi;
-        }
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        int i=0;
+         int j =0;
+         int longestsubstr=0;
+         unordered_map<char,int>mp;
+         while(j<s.size())
+         {
+             mp[s[j]]++;
+              if((j-i+1)<mp.size())
+              {
+                  j++;
+              }
+             else if((j-i+1)==mp.size())
+             {
+              longestsubstr=max(longestsubstr,j-i+1);   
+                 j++;
+             }
+             else {
+                 while((j-i+1)>mp.size())
+                 {
+                   mp[s[i]]--;
+                     if(mp[s[i]]==0)
+                     {
+                         mp.erase(s[i]);
+                     }
+                     i++;
+                     
+                 }
+                 j++;
+             }
+          
+          
+         }
+            return longestsubstr;
+    }
 };
