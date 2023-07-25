79dcf77e03781928d58da50755c52cc78ebfaa14
SAYAN MAITRA <75684139+sayan112@users.noreply.github.com>
2023-07-25 23:31:11 +0530

    Time: 0 ms (100.00%), Space: 10.5 MB (77.22%) - LeetHub

--- 0833-find-and-replace-in-string/0833-find-and-replace-in-string.cpp
+++ 0833-find-and-replace-in-string/0833-find-and-replace-in-string.cpp
@@ -1,23 +1,33 @@
-class Solution {
-public:
-    string findReplaceString(string s, vector<int>& indices, vector<string>& sources, vector<string>& targets) {
+class Solution
+{
+    public:
+        string findReplaceString(string s, vector<int> &indices, vector<string> &sources, vector< string > &targets)
+        {
             vector<pair<int, int>> sortedIndices;
-        for (int i = 0; i < indices.size(); ++i) {
-            sortedIndices.emplace_back(indices[i], i);
-        }
-        sort(sortedIndices.rbegin(), sortedIndices.rend());
-
-        for (const auto& pair : sortedIndices) {
-            int index = pair.first;
-            int i = pair.second;
-            const string& source = sources[i];
-            const string& target = targets[i];
-
-            if (s.substr(index, source.size()) == source) {
-                s.replace(index, source.size(), target);
+            for (int i = 0; i < indices.size(); ++i)
+            {
+                sortedIndices.emplace_back(indices[i], i);
             }
-        }
+            sort(sortedIndices.rbegin(), sortedIndices.rend());
+               
+            // The sorting of indices in reverse order ensures that replacements are made from right to left, preventing issues where replacements might affect the position of other indices to be replaced.
+            
+            
+            for (auto elem: sortedIndices)
+            {
+                int index = elem.first;
+                int i = elem.second;
+                string source = sources[i];
+                string target = targets[i];
+                cout << source << " " << target << endl;
 
-        return s;
-    }
+                if (s.substr(index, source.size()) == source)
+                {
+                    s.replace(index, source.size(), target);
+                }
+            }
+
+         
+            return s;
+        }
 };
