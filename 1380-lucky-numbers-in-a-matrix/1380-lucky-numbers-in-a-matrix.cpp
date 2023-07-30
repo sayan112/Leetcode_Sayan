26ea18b2cfc980ec0d37ae70cfccadbab4581c92
SAYAN MAITRA <75684139+sayan112@users.noreply.github.com>
2023-07-30 22:45:16 +0530

    Time: 14 ms (99.50%), Space: 11.4 MB (63.32%) - LeetHub

--- 1380-lucky-numbers-in-a-matrix/1380-lucky-numbers-in-a-matrix.cpp
+++ 1380-lucky-numbers-in-a-matrix/1380-lucky-numbers-in-a-matrix.cpp
@@ -1,41 +1,23 @@
 class Solution {
 public:
     vector<int> luckyNumbers (vector<vector<int>>& matrix) {
+   
         vector<int>ans;
-         vector<int>minirow;
+         vector<int>minirow(matrix.size(),INT_MAX);
+             vector<int>maxicol(matrix[0].size(),INT_MIN);
+        
          for(int i =0;i< matrix.size();i++)
          {
-              int mini =INT_MAX;
+             
                  for(int j =0;j< matrix[0].size();j++)
          {
-            if(mini>=matrix[i][j])
-            {
-                mini = matrix[i][j];
-            }
+             minirow[i]=min( minirow[i], matrix[i][j]);
+                     maxicol[j]=max(maxicol[j],matrix[i][j]);
          }
-              minirow.push_back(mini);
-              // cout << endl;
+            
          }
         
-         vector<int>maxicol;
-        
-               for(int j =0;j< matrix[0].size();j++)
-         {
-                      int maxi =INT_MIN;
-                 for(int i =0;i< matrix.size();i++)
-         {
-             if(maxi<=matrix[i][j])
-            {
-                maxi = matrix[i][j];
-            }
-         }
-                   maxicol.push_back(maxi);
-         }
-        
-         // for(auto elem : maxicol)
-         // {
-         //      cout << elem <<" ";
-         // }
+    
         
          for(int i =0; i< matrix.size();i++)
          {
