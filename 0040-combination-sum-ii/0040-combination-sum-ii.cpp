95f2e17b0f2c23408ead19bd752ff63ae0e2a138
SAYAN MAITRA <75684139+sayan112@users.noreply.github.com>
2023-08-22 03:25:04 +0530

    Time: 3 ms (87.21%), Space: 10.6 MB (51.77%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -1,12 +1,11 @@
 class Solution
 {
     public:
-        void uniquecombination(int idx, vector<int> &candidates, int target, vector<int> &ds, vector< vector< int>> &ans)
+        void generateCombinationsum2(vector<int> &candidates, int idx, vector<int> &ds, vector< vector< int>> &ans, int target)
         {
             if (target == 0)
             {
                 ans.push_back(ds);
-                return;
             }
             for (int i = idx; i < candidates.size(); i++)
             {
@@ -19,17 +18,17 @@ class Solution
                     break;
                 }
                 ds.push_back(candidates[i]);
-                uniquecombination(i + 1, candidates, target - candidates[i], ds, ans);
+                generateCombinationsum2(candidates, i + 1, ds, ans, target - candidates[i]);
                 ds.pop_back();
             }
         }
     vector<vector < int>> combinationSum2(vector<int> &candidates, int target)
     {
         sort(candidates.begin(), candidates.end());
-        int idx = 0;
         vector<vector < int>> ans;
         vector<int> ds;
-        uniquecombination(idx, candidates, target, ds, ans);
-        return ans;
+        int idx = 0;
+        generateCombinationsum2(candidates, idx, ds, ans, target);
+         return ans;
     }
 };
