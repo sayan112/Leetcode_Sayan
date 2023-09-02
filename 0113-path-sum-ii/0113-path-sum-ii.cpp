937112757f4732b43f2f515e63d6bb6205289dc7
SAYAN MAITRA <75684139+sayan112@users.noreply.github.com>
2023-09-02 17:13:50 +0530

    Time: 16 ms (35.04%), Space: 39 MB (8.07%) - LeetHub

--- 0113-path-sum-ii/0113-path-sum-ii.cpp
+++ 0113-path-sum-ii/0113-path-sum-ii.cpp
@@ -1,44 +1,41 @@
 /**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
+ *Definition for a binary tree node.
+ *struct TreeNode {
+ *   int val;
+ *   TreeNode * left;
+ *   TreeNode * right;
+ *   TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *   TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *   TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *};
  */
-class Solution {
-public:
-    void findsum(TreeNode* root, int targetSum, vector<int>ds,  vector<vector<int>>&ans)
-    {
-         if(!root)
-         {
-              return ;
-         }
-        
-       
-        if(root->left==NULL && root->right==NULL)
+class Solution
+{
+    public:
+        void countingPathSum(TreeNode *root, int targetSum, vector<int> vec, vector<vector< int>> &ans)
         {
-                   
-            if(targetSum-root->val==0)
+            if (!root)
             {
-                      ds.push_back(root->val);
-                ans.push_back(ds);
-                 return ;
+                return;
             }
+            vec.push_back(root->val);
+            targetSum-=root->val;
+            if (!root->left && !root->right)
+            {
+                if (targetSum == 0)
+                {
+                    ans.push_back(vec);
+                }
+            }
+     
+            countingPathSum(root->left, targetSum, vec, ans);
+            countingPathSum(root->right, targetSum, vec, ans);
         }
-    
-              ds.push_back(root->val);
-        findsum(root->left,targetSum-root->val, ds , ans);
-        findsum(root->right,targetSum-root->val, ds , ans);
-         ds.pop_back();
-    }
-    vector<vector<int>> pathSum(TreeNode* root, int targetSum) {
-           vector<vector<int>>ans;
-        vector<int>ds;
-        findsum(root,targetSum,ds,ans);
-         return ans ;
+    vector<vector < int>> pathSum(TreeNode *root, int targetSum)
+    {
+        vector<vector < int>> ans;
+        vector<int> vec;
+        countingPathSum(root, targetSum, vec, ans);
+        return ans;
     }
 };
