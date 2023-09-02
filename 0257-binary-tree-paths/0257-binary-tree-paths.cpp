731019e3112f137ed6007b9400571a3463428948
SAYAN MAITRA <75684139+sayan112@users.noreply.github.com>
2023-09-02 18:11:15 +0530

    Time: 3 ms (71.86%), Space: 15.4 MB (6.19%) - LeetHub

--- 0257-binary-tree-paths/0257-binary-tree-paths.cpp
+++ 0257-binary-tree-paths/0257-binary-tree-paths.cpp
@@ -1,47 +1,47 @@
-class Solution {
-public:
-    
-     void allpaths(TreeNode* root, vector<int>need,   vector<vector<int>>&ans)
-     {
-          if(root==NULL)
-          {
-              return ; 
-          }
-          need.push_back(root->val);
-         if(root->right==NULL && root->left==NULL)
-         {
-             ans.push_back(need);
-         }
-          
-          allpaths(root->left,need,ans);
-          allpaths(root->right,need,ans);
-         
-     }
-    vector<string> binaryTreePaths(TreeNode* root) {
-          vector<string>op;
-        vector<int>need;
-         vector<vector<int>>ans;
-        
-    allpaths(root , need,ans);
-         for(auto elem : ans )
-         {
-              string ans1;
-             for( int i =0;i<elem.size();i++)
-             {
-                   ans1+=to_string(elem[i]);
-                  if(i!=elem.size()-1)
-                  {
-                      
-                 ans1+='-';
-                    ans1+='>';
-                  }
-               
-             }
-             op.push_back(ans1);
-            
-         }
-         return op; 
-        
-        
+/**
+ *Definition for a binary tree node.
+ *struct TreeNode {
+ *   int val;
+ *   TreeNode * left;
+ *   TreeNode * right;
+ *   TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *   TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *   TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *};
+ */
+class Solution
+{
+    public:
+        void binaryTreeAllPaths(TreeNode *root, vector<string> &ans,  vector<int>subans)
+        {
+            if (!root)
+            {
+                return;
+            }
+            subans.push_back(root->val);
+            if (!root->left && !root->right)
+            {
+                string str;
+                for(auto elem : subans)
+                {
+                    str+=to_string(elem);
+                    str+='-';
+                    str+='>';
+                }
+                str.pop_back();
+                  str.pop_back();
+                ans.push_back(str);
+                
+            }
+
+            binaryTreeAllPaths(root->left, ans, subans);
+            binaryTreeAllPaths(root->right, ans, subans);
+        }
+    vector<string> binaryTreePaths(TreeNode *root)
+    {
+        vector<string> ans;
+         vector<int>subans;
+        binaryTreeAllPaths(root, ans, subans);
+        return ans ;
     }
 };
