9c1e9e574e2537821c0da71c98ff6b132e52b51e
SAYAN MAITRA <75684139+sayan112@users.noreply.github.com>
2023-09-03 14:40:34 +0530

    Time: 7 ms (8.51%), Space: 7.2 MB (88.59%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.cpp
+++ 0412-fizz-buzz/0412-fizz-buzz.cpp
@@ -3,26 +3,23 @@ class Solution
     public:
         vector<string> fizzBuzz(int n)
         {
-            vector<string> ans;
+            vector<string> ans(n);
             for (int i = 1; i <= n; i++)
             {
-                if ((i % 3 == 0) && (i % 5 == 0))
-                {
-                    ans.push_back("FizzBuzz");
-                }
-                else if (i % 3 == 0)
-                {
-                    ans.push_back("Fizz");
-                }
-                else if (i % 5 == 0)
-                {
-                    ans.push_back("Buzz");
-                }
-                else
-                {
-                    ans.push_back(to_string(i));
-                }
+              ans[i-1]= to_string(i);
             }
-             return ans ;
+            for (int i = 2; i < n; i += 3)
+            {
+                ans[i]="Fizz";
+            }
+            for (int i = 4; i <n; i += 5)
+            {
+                 ans[i]="Buzz";
+            }
+            for (int i = 14; i < n; i += 15)
+            {
+               ans[i]="FizzBuzz";
+            }
+            return ans;
         }
 };
