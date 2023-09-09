befd999ecf7e762faca27f9c6509da9d4ef35c38
SAYAN MAITRA <75684139+sayan112@users.noreply.github.com>
2023-09-09 23:30:49 +0530

    Time: 14 ms (32.78%), Space: 6 MB (27.59%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,25 +1,11 @@
 class Solution {
 public:
-      int  fibo ( int i , unordered_map<int,int>&mp)
-      {
-            if(i==0|| i==1)
-        {
-             return i; 
-        }
-           int curIdx= i ; 
-           if(mp.find(curIdx)!=mp.end())
-           {
-                return mp[curIdx];
-           }
-           
-        int need= fibo(i-1,mp)+fibo(i-2,mp);
-          mp[curIdx]=need%1000000007;
-        return       mp[curIdx];
-      }
     int fib(int n) {
-         unordered_map<int,int>mp;
-       return  fibo(n,mp);
-      
-        
+     
+         if(n==1||n==0)
+         {
+             return n;
+         }
+        return fib(n-1)+fib(n-2);
     }
 };
