6e085d7e16f585d6f81085bbf26968811adca714
SAYAN MAITRA <75684139+sayan112@users.noreply.github.com>
2023-09-13 02:21:13 +0530

    Time: 9 ms (61.31%), Space: 7.6 MB (6.82%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+     unordered_map<char,int>mp;
+  if(s.size()!=t.size())
+  {
+       return false;
+  }
+      for(auto elem : s)
+      {
+           mp[elem]++;
+      }
+       for(auto elem : t )
+       {
+           if(mp.find(elem)!=mp.end())
+           {
+               mp[elem]--;
+                if(mp[elem]==0)
+                {
+                    mp.erase(elem);
+                }
+           }
+       }
+        
+        return !mp.size();
+    }
+};
