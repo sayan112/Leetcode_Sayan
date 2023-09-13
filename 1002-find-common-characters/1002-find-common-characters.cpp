beeb9d9dd1548561796193e533e44f07f08e745e
SAYAN MAITRA <75684139+sayan112@users.noreply.github.com>
2023-09-13 10:25:54 +0530

    Time: 16 ms (28.81%), Space: 10.5 MB (25.09%) - LeetHub

---
+++ 1002-find-common-characters/1002-find-common-characters.cpp
@@ -0,0 +1,46 @@
+class Solution
+{
+    public:
+        vector<string> commonChars(vector<string> &words)
+        {
+            unordered_map<char, int> mp;
+            for (auto elem: words[0])
+            {
+                mp[elem]++;
+            }
+            unordered_map<char, int> mp1;
+            for (int i = 1; i < words.size(); i++)
+            {
+                mp1.clear();
+                for (auto elem: words[i])
+                {
+                    mp1[elem]++;
+                }
+
+                for (auto elem: mp)
+                {
+                    if (mp.find(elem.first) != mp.end())
+                    {
+                        mp[elem.first] = min(mp[elem.first], mp1[elem.first]);
+                    }
+                }
+            }
+            vector<string> ans;
+
+            for (auto elem: mp)
+            {
+                if (elem.second != 0)
+                {
+
+                    while (elem.second--)
+                    {
+                        string tempans;
+                        tempans += elem.first;
+                        ans.push_back(tempans);
+                    }
+                }
+            }
+
+            return ans;
+        }
+};
