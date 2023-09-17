a246de3a9d221d3c887381abc051b63526645291
SAYAN MAITRA <75684139+sayan112@users.noreply.github.com>
2023-09-17 19:25:23 +0530

    Time: 152 ms (93.05%), Space: 114.4 MB (65.92%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -1,26 +1,73 @@
 /**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
+ *Definition for singly-linked list.
+ *struct ListNode {
+ *    int val;
+ *    ListNode * next;
+ *    ListNode() : val(0), next(nullptr) {}
+ *    ListNode(int x) : val(x), next(nullptr) {}
+ *    ListNode(int x, ListNode *next) : val(x), next(next) {}
+ *};
  */
-class Solution {
-public:
-    bool isPalindrome(ListNode* head) {
-        string s;
-        
-        while(head)
+class Solution
+{
+    public:
+        ListNode* middleOfLinkedList(ListNode *temp)
         {
-           s+=(head->val)+'0';
-            head=head->next;
-
+            ListNode *temp1 = temp;
+            int size = 0;
+            while (temp)
+            {
+                size++;
+                temp = temp->next;
+            }
+            int middle = size / 2;
+            while (middle--)
+            {
+                temp1 = temp1->next;
+            }
+             if(size%2==1)
+             {
+                    return temp1->next;
+             }
+            return temp1;
         }
-       string check = s;
-         reverse(check.begin(),check.end());
-         return check==s;
+    ListNode* reverse(ListNode *head)
+    {
+
+        ListNode *newhead = NULL;
+        while (head)
+        {
+            ListNode *next = head->next;
+            head->next = newhead;
+            newhead = head;
+            head = next;
+        }
+        return newhead;
+    }
+    bool isPalindrome(ListNode *head)
+    {
+        ListNode *temp = head;
+        ListNode *middle = middleOfLinkedList(temp);
+         temp=head;
+       ListNode * reversenode=   reverse(middle);
+         // while(reversenode)
+         // {
+         //      cout << reversenode->val;
+         //     reversenode=reversenode->next;
+         // }
+ while(reversenode)
+ {
+     if(reversenode->val!=temp->val)
+     {
+         cout << reversenode->val<<" "<<temp->val<<endl;
+       
+          return false;
+     }
+     else{
+         reversenode=reversenode->next;
+         temp=temp->next;
+     }
+ }
+        return true;
     }
 };
