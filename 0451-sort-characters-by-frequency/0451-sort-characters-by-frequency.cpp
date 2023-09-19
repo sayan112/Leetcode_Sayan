c9a373e1cb8802002320986172b061053aee01d5
SAYAN MAITRA <75684139+sayan112@users.noreply.github.com>
2023-09-19 11:36:50 +0530

    Time: 13 ms (38.26%), Space: 8.8 MB (21.58%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -1,32 +1,29 @@
-class Solution
-{
-    public:
-        string frequencySort(string s)
-        {
-            unordered_map<char, int> mp;
-            for (auto elem: s)
-            {
-                mp[elem]++;
-            }
+class Solution {
+public:
 
-            priority_queue<pair<int, char>> q;
-            for (auto elem: mp)
-            {
-                q.push({ elem.second,
-                    elem.first });
-            }
-
-            string ans;
-            while (q.size() != 0)
-            {
-                auto it = q.top();
-                q.pop();
-                cout << it.first << " " << it.second << endl;
-                while (it.first--)
-                {
-                    ans += it.second;
-                }
-            }
-            return ans;
-        }
+    string frequencySort(string s) {
+        unordered_map<char,int>mp;
+         for(auto elem : s)
+         {
+             mp[elem]++;
+         }
+        
+        priority_queue<pair<int,int>>pq;
+        
+    for(auto elem : mp)
+    {
+        pq.push({elem.second,elem.first});
+    }
+         string ans;
+         while(pq.size())
+         {
+              auto elem =pq.top();
+             while(elem.first--)
+             {
+                ans+=elem.second;
+             }
+             pq.pop();
+         }
+     return ans;   
+    }
 };
