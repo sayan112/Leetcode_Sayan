8d78ae8e84cc0047d08b7ab01307a2f0265776d4
SAYAN MAITRA <75684139+sayan112@users.noreply.github.com>
2023-09-23 11:31:47 +0530

    Time: 8 ms (48.74%), Space: 19.1 MB (7.34%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
@@ -1,25 +1,43 @@
 /**
- *Definition for a binary tree node.
- *struct TreeNode {
- *    int val;
- *    TreeNode * left;
- *    TreeNode * right;
- *    TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- *};
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
  */
-class Solution
-{
-    public:
-        int maxDepth(TreeNode *root)
-        {
-            if (!root)
-            {
-                return 0;
-            }
-            int leftheight = maxDepth(root->left);
-            int rightheight = maxDepth(root->right);
-            return 1 + max(leftheight, rightheight);
-        }
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+         int height=0;
+        queue<TreeNode*>q;
+         if(!root)
+         {
+              return height;
+         }
+         q.push(root);
+         while(q.size())
+         {
+             int size=q.size();
+              height++;
+              for(int i =0;i<size;i++)
+              {
+                  TreeNode*node =q.front();
+                  q.pop();
+                  if(node->left)
+                  {
+                      q.push(node->left);
+                  }
+                  if(node->right)
+                  {
+                      q.push(node->right);
+                  }
+              }
+             
+         }
+         return height;
+    }
 };
