be18253ae49e702bab3e2892c7ba7a38143220fb
SAYAN MAITRA <75684139+sayan112@users.noreply.github.com>
2023-09-24 20:13:44 +0530

    Time: 19 ms (19.71%), Space: 8.4 MB (33.70%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+        
+        
+      //  XXVII // largest to smallest  2+5+10+10
+        // XIV // 5+(-1)
+        
+         unordered_map<char,int>mp;
+     mp['I']=1;
+        mp['V']=5;
+            mp['X']=10;
+            mp['L']=50;
+            mp['C']=100;
+            mp['D']=500;
+            mp['M']=1000;
+        int ans =mp[s.back()];
+            
+         for(int i=s.size()-2;i>=0;i--)
+         {
+             if(mp[s[i]]<mp[s[i+1]])
+             {
+                 ans-=mp[s[i]];
+             }
+             else{
+                 ans+=mp[s[i]];
+             }
+         }
+         return ans;
+        
+        
+        
+        
+    }
+};
