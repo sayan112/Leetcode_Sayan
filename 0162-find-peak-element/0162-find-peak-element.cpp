ad7fb39db135566efa88e106752da7b3b56dea64
SAYAN MAITRA <75684139+sayan112@users.noreply.github.com>
2023-09-24 20:56:24 +0530

    Time: 3 ms (67.29%), Space: 9.3 MB (5.01%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -1,55 +1,31 @@
-class Solution
-{
-    public:
-        int findPeakElement(vector<int> &nums)
-        {
-
-            int start = 0;
-           int end = nums.size()-1;
-             if(nums.size()==1)
-             {
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+         if(nums.size()==1)
+         {
+              return 0;
+         }
+         if(nums.size()==2)
+         {
+              if(nums[0]>nums[1])
+              {
                   return 0;
+              }
+             else{
+                  return 1;
              }
-            while (end >= start)
-            {
-                int mid = start + (end - start) / 2;
-                if (mid > 0 && mid < nums.size() - 1)
-                {
-
-                    if (nums[mid] > nums[mid - 1] && nums[mid] > nums[mid + 1])
-                    {
-                        return mid;
-                    }
-                    else if (nums[mid] < nums[mid - 1])
-                    {
-
-                        end = mid - 1;
-                    }
-                    else
-                    {
-                        start = mid + 1;
-                    }
-                }
-                 else if(mid==0)
-                 {
-                     if(nums[0]>nums[1])
-                     {
-                         return 0;
-                     }
-                     else{
-                         return 1;
-                     }
-                 }
-                else if(mid=nums.size() - 1){
-                     if(nums[mid]>nums[nums.size()-2])
-                     {
-                         return mid ;
-                     }
-                     else{
-                         return nums.size()-2 ;
-                     }
-                }
-            }
-            return 0;
-        }
+         }
+     for(int i =1;i<nums.size()-1;i++)
+     {
+          if(nums[i]>nums[i-1] && nums[i]>nums[i+1])
+          {
+               return i;
+          }
+     }
+ if(nums[nums.size()-1]>nums[nums.size()-2])
+ {
+     return nums.size()-1;
+ }
+        return 0;
+    }
 };
