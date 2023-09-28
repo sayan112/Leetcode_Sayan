6c6658b09fb99b1ba93d8497561415d88fc2fb75
SAYAN MAITRA <75684139+sayan112@users.noreply.github.com>
2023-09-28 23:20:49 +0530

    Time: 0 ms (100.00%), Space: 6.9 MB (6.71%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,33 +1,9 @@
-class Solution
-{
-    public:
-        double myPow(double x, int n)
-        {
-            double sum = 1.00;
-            long need = n;
-            if (n < 0)
-            {
-                need = (double)-1* n;
-            }
-            while (need > 0)
-            {
-                if (need % 2 == 1)
-                {
-                    sum = sum * x;
-                    need = need - 1;
-                }
-                else
-                {
-                    x = x * x;
-                    need = need / 2;
-                }
-            }
-             if(n<0)
-             {
-                  return (double)1.00/(double)sum;
-             }
-            else{
-                 return sum;
-            }
-        }
+class Solution {
+public:
+    double myPow(double x, int n) {
+   
+        
+        
+        return pow(x,n);
+    }
 };
