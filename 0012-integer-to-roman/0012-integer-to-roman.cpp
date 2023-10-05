813153e23c3d6be91d50830c6b0a8735191dcdec
SAYAN MAITRA <75684139+sayan112@users.noreply.github.com>
2023-10-06 01:04:53 +0530

    Time: 17 ms (10.00%), Space: 9.9 MB (13.40%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.cpp
+++ 0012-integer-to-roman/0012-integer-to-roman.cpp
@@ -1,16 +1,24 @@
 class Solution {
 public:
     string intToRoman(int num) {
-        map<int,string> mp{{1,"I"},{4,"IV"},{5,"V"},{9,"IX"},{10,"X"},{40,"XL"},{50,"L"},{90,"XC"},{100,"C"},{400,"CD"},{500,"D"},{900,"CM"},{1000,"M"}};
-         string ans;
- while(num!=0)
- {
-    auto elem=mp.upper_bound(num);
-     elem--;
-     num-=elem->first;
-     ans+=elem->second;
-     
- }
+        vector<pair<int,string>>pr{{1,"I"},{4,"IV"},{5,"V"},{9,"IX"},{10,"X"},{40,"XL"},{50,"L"},{90,"XC"},{100,"C"},{400,"CD"},{500,"D"},{900,"CM"},{1000,"M"}};
+        reverse(pr.begin(),pr.end());
+        string ans;
+     while(num!=0)
+     {
+         for(auto elem : pr)
+         {
+             if(num>=elem.first)
+             {
+                 num-=elem.first;
+                 ans+=elem.second;
+                 break;
+             }
+         }
+     }
          return ans;
+        
+        
+    
     }
 };
