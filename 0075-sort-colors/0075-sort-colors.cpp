b5da33c6c12409a23c7f6716067d60ce70424bfb
SAYAN MAITRA <75684139+sayan112@users.noreply.github.com>
2023-10-08 00:43:47 +0530

    Time: 0 ms (100.00%), Space: 8.6 MB (26.74%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -6,17 +6,17 @@ public:
          int high=nums.size()-1;
          while(mid<=high)
          {
-             if(nums[mid]==0)
+             if(nums[mid]==0)// when mid pointing to 0 , swap mid , low , do mid++ , low++
              {
                  swap(nums[mid],nums[low]);
                  mid++;
                  low++;
              }
-             else if(nums[mid]==1)
+             else if(nums[mid]==1)// when mid at 1 , just do mid++
              {
                  mid++;
              }
-             else{
+             else{ // when mid at 2 , just do swap mid, high , and high--
                  swap(nums[mid],nums[high]);
                  high--;
              }
