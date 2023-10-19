b097f3fc453c253d5c039e18b47fc9df3e18a802
SAYAN MAITRA <75684139+sayan112@users.noreply.github.com>
2023-10-20 01:57:14 +0530

    Time: 16 ms (18.85%), Space: 13.9 MB (27.77%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,53 +1,50 @@
 class Solution {
 public:
-    void setmatrixrow ( int i ,vector<vector<int>>& matrix , int col  )
-    {
-         for(int j =0; j < col ;j++)
+     void setRow(int row ,vector<vector<int>>& matrix )
+     {
+         for(int i =0;i<matrix[0].size();i++)
          {
-              if(matrix[i][j]!=0)
-              {
-                   matrix[i][j]=-100000011;
-              }
+             if(   matrix[row][i]!=0)
+             {
+                    matrix[row][i]=-138423; 
+             }
+         
          }
-        
-    }
-      void setmatrixcol ( int j ,vector<vector<int>>& matrix , int row )
-    {
-         for(int i =0; i < row ;i++)
+     }
+     void setCol(int col ,vector<vector<int>>& matrix )
+     {
+         for(int i =0;i<matrix.size();i++)
          {
-              if(matrix[i][j]!=0)
-              {
-                   matrix[i][j]=-100000011;
-              }
+             if( matrix[i][col]!=0)
+             {
+                       matrix[i][col]=-138423;
+             }
          }
-        
-    }
+     }
     void setZeroes(vector<vector<int>>& matrix) {
-       
-        int row = matrix.size();
-         int col = matrix[0].size();
-         for(int i =0;i<row;i++)
+     
+         for(int i=0;i<matrix.size();i++)
          {
-              for(int j =0;j<col;j++)
+              for(int j=0;j<matrix[0].size();j++)
          {
-              if(matrix[i][j]==0)
-              {
-                  setmatrixrow(i,matrix,col);
-                  setmatrixcol(j,matrix,row);
-              }
-         }  
+             if(matrix[i][j]==0)
+             {
+                 setRow(i,matrix);
+                 setCol(j,matrix);
+             }
+         } 
          }
         
-              for(int i =0;i<row;i++)
+        
+             for(int i=0;i<matrix.size();i++)
          {
-              for(int j =0;j<col;j++)
-         {
-                  if(matrix[i][j]==-100000011)
+              for(int j=0;j<matrix[0].size();j++)
+              {
+                  if(matrix[i][j]==-138423)
                   {
-                    matrix[i][j]=0;   
+                      matrix[i][j]=0;
                   }
-              }
-              }
+              }}
         
     }
 };
