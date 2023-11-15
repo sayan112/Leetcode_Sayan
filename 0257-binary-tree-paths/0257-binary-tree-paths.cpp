2210ed86d8254b686baae0955c7913dda687e4e9
SAYAN MAITRA <75684139+sayan112@users.noreply.github.com>
2023-11-15 22:15:41 +0530

    Time: 4 ms (51.11%), Space: 14 MB (61.93%) - LeetHub

--- 0257-binary-tree-paths/0257-binary-tree-paths.cpp
+++ 0257-binary-tree-paths/0257-binary-tree-paths.cpp
@@ -1,47 +1,50 @@
 /**
- *Definition for a binary tree node.
- *struct TreeNode {
- *   int val;
- *   TreeNode * left;
- *   TreeNode * right;
- *   TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *   TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *   TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- *};
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
  */
-class Solution
-{
-    public:
-        void binaryTreeAllPaths(TreeNode *root, vector<string> &ans,  vector<int>subans)
-        {
-            if (!root)
-            {
-                return;
-            }
-            subans.push_back(root->val);
-            if (!root->left && !root->right)
-            {
-                string str;
-                for(auto elem : subans)
-                {
-                    str+=to_string(elem);
-                    str+='-';
-                    str+='>';
-                }
-                str.pop_back();
-                  str.pop_back();
-                ans.push_back(str);
-                
-            }
-
-            binaryTreeAllPaths(root->left, ans, subans);
-            binaryTreeAllPaths(root->right, ans, subans);
-        }
-    vector<string> binaryTreePaths(TreeNode *root)
-    {
-        vector<string> ans;
-         vector<int>subans;
-        binaryTreeAllPaths(root, ans, subans);
-        return ans ;
+class Solution {
+public:
+     void binaryTreePath (TreeNode* root , string need , vector<string>&ans  )
+     {
+         if(!root)
+         {
+             return ;
+         }
+          if(!root->left && !root->right)
+          {
+   
+          need+=to_string(root->val);
+              ans.push_back(need);
+               return ;
+          }
+         
+          need+=to_string(root->val);
+            need+="->";
+         
+         
+         binaryTreePath(root->left,need,ans);
+                  
+         binaryTreePath(root->right,need,ans);
+      
+         
+     }
+    vector<string> binaryTreePaths(TreeNode* root) {
+     
+          vector<string>ans;
+         string need;
+        
+        binaryTreePath(root,need,ans);
+         for(auto elem : ans )
+         {
+              cout << elem <<" ";
+         }
+         return ans ;
     }
 };
