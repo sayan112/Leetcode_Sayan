69bf085092a4ecc310c078fdbfbd06083956ea9d
SAYAN MAITRA <75684139+sayan112@users.noreply.github.com>
2023-11-18 17:08:44 +0530

    Time: 0 ms (100.00%), Space: 6.7 MB (32.05%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,41 +1,38 @@
-class Solution
-{
-    public:
-        bool isValid(string s)
-        {
-            stack<char> st;
-            for (auto elem: s)
-            {
-                if (elem == '(' || elem == '{' || elem == '[')
-                {
-                    st.push(elem);
-                }
-                else
-                {
-                    if (st.size())
-                    {
-
-                        char check = st.top();
-                        st.pop();
-                        if ((check == '(' && elem == ')') || (check == '{' && elem == '}') || (check == '[' && elem == ']'))
-                        {
-                            continue;
-                        }
-                        else
-                        {
-                            return false;
-                        }
-                    }
-                    else
-                    {
-                        return false;
-                    }
-                }
-            }
-            if (st.size())
-            {
-                return false;
-            }
-            return true;
-        }
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char>st;
+         for(auto elem : s )
+         {
+              if(elem=='('|| elem=='['||elem=='{')
+              {
+                  st.push(elem);
+              }
+             else{
+                  if(st.size())
+                  {
+                       auto topelem = st.top();
+                       st.pop();
+                  if((topelem=='('&&elem==')')||(topelem=='['&&elem==']')||(topelem=='{'&&elem=='}'))
+                  {
+                     
+                    continue;
+                  }
+                 else{
+                     return false;
+                 }
+                  }
+                 else{
+                      return false;
+                 }
+                
+             }
+         }
+      
+        if(st.empty())
+         {
+             return true;
+         }
+         return false;
+    }
 };
