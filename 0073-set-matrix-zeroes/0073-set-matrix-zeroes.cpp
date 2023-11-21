fbd134deaad9c2c62a02332264a77b23c7b65486
SAYAN MAITRA <75684139+sayan112@users.noreply.github.com>
2023-11-21 14:35:16 +0530

    Time: 7 ms (91.61%), Space: 13.8 MB (75.57%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,50 +1,52 @@
-class Solution {
-public:
-     void setRow(int row ,vector<vector<int>>& matrix )
-     {
-         for(int i =0;i<matrix[0].size();i++)
-         {
-             if(   matrix[row][i]!=0)
-             {
-                    matrix[row][i]=-138423; 
-             }
-         
-         }
-     }
-     void setCol(int col ,vector<vector<int>>& matrix )
-     {
-         for(int i =0;i<matrix.size();i++)
-         {
-             if( matrix[i][col]!=0)
-             {
-                       matrix[i][col]=-138423;
-             }
-         }
-     }
-    void setZeroes(vector<vector<int>>& matrix) {
-     
-         for(int i=0;i<matrix.size();i++)
-         {
-              for(int j=0;j<matrix[0].size();j++)
-         {
-             if(matrix[i][j]==0)
-             {
-                 setRow(i,matrix);
-                 setCol(j,matrix);
-             }
-         } 
-         }
-        
-        
-             for(int i=0;i<matrix.size();i++)
-         {
-              for(int j=0;j<matrix[0].size();j++)
-              {
-                  if(matrix[i][j]==-138423)
-                  {
-                      matrix[i][j]=0;
-                  }
-              }}
-        
+class Solution
+{
+    public:
+        void setRow(vector<vector < int>> &matrix, int fixrow)
+        {
+            for (int i = 0; i < matrix[0].size(); i++)
+            {
+                if(  matrix[fixrow][i]!=0)
+                {
+                         matrix[fixrow][i] = -19999;
+                }
+           
+            }
+        }
+    void setCol(vector<vector < int>> &matrix, int fixCol)
+    {
+        for (int i = 0; i < matrix.size(); i++)
+        {
+            
+                if(   matrix[i][fixCol]!=0)
+                {
+                         matrix[i][fixCol] = -19999;
+                }
+          
+        }
+    }
+    void setZeroes(vector<vector < int>> &matrix)
+    {
+        for (int i = 0; i < matrix.size(); i++)
+        {
+            for (int j = 0; j < matrix[0].size(); j++)
+            {
+                if (matrix[i][j] == 0)
+                {
+                    setRow(matrix, i);
+                    setCol(matrix, j);
+                }
+            }
+        }
+
+        for (int i = 0; i < matrix.size(); i++)
+        {
+            for (int j = 0; j < matrix[0].size(); j++)
+            {
+                if (matrix[i][j] == -19999)
+                {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
     }
 };
