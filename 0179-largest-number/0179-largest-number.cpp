781c6f04413c297128944beb1972f1a49a565c81
SAYAN MAITRA <75684139+sayan112@users.noreply.github.com>
2023-11-22 11:33:23 +0530

    Time: 6 ms (53.63%), Space: 11.6 MB (72.25%) - LeetHub

--- 0179-largest-number/0179-largest-number.cpp
+++ 0179-largest-number/0179-largest-number.cpp
@@ -1,26 +1,24 @@
-class compare{
-  public:
-        bool operator()(int a, int b){
-            string s1 = to_string(a), s2=to_string(b);
-            return s1+s2 > s2+s1;
-        }
-};
 class Solution {
 public:
-    
+   
+    static bool compare (int a , int b )
+    {
+        string need1= to_string(a)+to_string(b);
+        string need2=to_string(b)+to_string(a);
+        return need1>need2;
+    }
     string largestNumber(vector<int>& nums) {
-     sort(nums.begin(),nums.end(),compare());
-         string ans;
-         for(auto elem : nums)
-         {
-             ans+=to_string(elem);
-
-         }
+      sort(nums.begin(),nums.end(),compare) ; 
+     
+        string ans;
+        for(auto elem : nums)
+        {
+            ans+=to_string(elem);
+        }
          if(nums[0]==0)
          {
               return "0";
          }
-         return ans;
-        
+         return ans ;
     }
 };
