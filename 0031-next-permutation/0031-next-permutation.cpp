843caa8e47ee901eb378181603e25ae5fa255840
SAYAN MAITRA <75684139+sayan112@users.noreply.github.com>
2023-11-30 16:41:48 +0530

    Time: 4 ms (50.48%), Space: 12.4 MB (54.07%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,7 +1,56 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        next_permutation(nums.begin(),nums.end());
-         
+ //[2,3,0,5,3,2,1]
+ //[2,3,1,0,2,3,5]       
+        
+        
+        if(nums.size()==1)
+        {
+            return;
+        }
+        else if(nums.size()==2)
+        {
+            reverse(nums.begin(),nums.end());
+            return;
+        }
+        else{
+             int idx =-1;
+               for(int i =nums.size()-2;i>=0;i--)
+               {
+                   if(nums[i]<nums[i+1])
+                   {
+                       idx=i;
+                       break;
+                   }
+               }
+             if(idx<0)
+             {
+                
+            reverse(nums.begin(),nums.end());
+                 return;
+             }
+    
+             int replaceInt=nums[idx+1];
+       
+             int replaceidx=-1;
+            for(int i=idx+1;i<nums.size();i++)
+            {
+                if(nums[idx]<nums[i])
+                {
+                    replaceInt=min(nums[i],replaceInt);
+                  
+                    if(replaceInt==nums[i])
+                    {
+                        replaceidx=i;
+                    }
+                }
+            }
+           
+           swap(nums[idx],nums[replaceidx]);
+         reverse(nums.begin()+idx+1,nums.end());
+             
+        }
+         return;
     }
 };
