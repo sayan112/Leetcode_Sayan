514eacbc33aea71747380a09da285bba188b0bb9
SAYAN MAITRA <75684139+sayan112@users.noreply.github.com>
2023-12-05 02:46:50 +0530

    Time: 12 ms (32.46%), Space: 7.9 MB (8.94%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,27 +1,37 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-     unordered_map<char,int>mp;
-  if(s.size()!=t.size())
-  {
-       return false;
-  }
-      for(auto elem : s)
-      {
-           mp[elem]++;
-      }
-       for(auto elem : t )
-       {
-           if(mp.find(elem)!=mp.end())
-           {
-               mp[elem]--;
-                if(mp[elem]==0)
-                {
-                    mp.erase(elem);
-                }
-           }
-       }
+            unordered_map<char,int>mp;
+        if(s.size()!=t.size())
+        {
+            return false;
+        }
+        else{
         
-        return !mp.size();
+             for(auto elem : s)
+             {
+                 mp[elem]++;
+             }
+         
+             for(auto elem : t)
+             {
+                 if(mp.find(elem)!=mp.end())
+                 {
+                     mp[elem]--;
+                      if(mp[elem]==0)
+                      {
+                          mp.erase(elem);
+                      }
+                 }
+                 else{
+                      return false;
+                 }
+             }
+        }
+        if(mp.size()==0)
+        {
+            return true;
+        }
+        return false;
     }
 };
