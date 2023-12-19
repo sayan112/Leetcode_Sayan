e9d0060e2cd4ea2a1607bbd09349da4821075322
SAYAN MAITRA <75684139+sayan112@users.noreply.github.com>
2023-12-19 09:40:55 +0530

    Time: 4 ms (31.51%), Space: 8.6 MB (36.99%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,33 +1,34 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        int count0 =0;
+        int count2=0;
          int count1=0;
-         int count2=0;
-     for(auto elem : nums)
-     {
-          if(elem==0)
-          {
-              count0++; 
-          }else if(elem==1)
-          {
-              count1++;
-          }
-         else{
-              count2++;
-         }
-     }
+         int count0=0;
+       for(auto elem : nums)
+       {
+            if(elem==2)
+            {
+                count2++;
+            }
+           else if (elem==1)
+           {
+               count1++;
+           }
+           else {
+               count0++;
+               
+           }
+       }
          nums.clear();
-        while(count0--)
-        {
-            nums.push_back(0);
-        }
-            while(count1--)
-        {
+         while(count0--)
+         {
+             nums.push_back(0);
+         }
+        while(count1--){
             nums.push_back(1);
         }
-            while(count2--)
-        {
+        
+           while(count2--){
             nums.push_back(2);
         }
     }
