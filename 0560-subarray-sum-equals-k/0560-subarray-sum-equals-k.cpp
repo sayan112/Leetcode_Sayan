c557491b9d0999f5378c237e5878186bf7d5124e
SAYAN MAITRA <75684139+sayan112@users.noreply.github.com>
2023-12-26 18:42:07 +0530

    Time: 84 ms (32.95%), Space: 47.3 MB (30.68%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -9,7 +9,7 @@ class Solution
              int sum =0;
              int count =0;
              for(auto elem : A)
-             {
+             { 
                  sum+=elem;
                   count+=mp[sum-k];
                   mp[sum]++;
