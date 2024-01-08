17d3d2325ecb3d8788ad314476526e138c16fecb
SAYAN MAITRA <75684139+sayan112@users.noreply.github.com>
2024-01-08 16:23:26 +0530

    Time: 12 ms (93.48%), Space: 19.5 MB (97.08%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,30 +1,20 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-         int zerocount=0;
-        
-        for(auto elem : nums)
-        {
-            if(elem==0)
-            {
-                zerocount++;
-            }
-        }
-        
-      vector<int>nonzeroelem;
-         for(auto elem : nums)
+         int left=0;
+        int right=0;
+         while(right!=nums.size())
          {
-             if(elem!=0)
+             if(nums[right]!=0)
              {
-                 nonzeroelem.push_back(elem);
+                 swap(nums[right],nums[left]);
+                 right++;
+                 left++;
+             }
+             else{
+                 right++;
              }
          }
-        while(zerocount--)
-        {
-             nonzeroelem.push_back(0);
-        }
-        nums.clear();
-        nums=nonzeroelem;
         
     }
 };
