da9453897f5987d12e4973494e9ab33e44f0b328
SAYAN MAITRA <75684139+sayan112@users.noreply.github.com>
2024-01-10 01:16:12 +0530

    Time: 3 ms (42.34%), Space: 6.8 MB (74.33%) - LeetHub

---
+++ 0394-decode-string/0394-decode-string.cpp
@@ -0,0 +1,42 @@
+class Solution
+{
+    public:
+        string Decode(string &s, int &i)
+        {
+            cout<< i<<" ";
+            string result;
+            while (i < s.size() && s[i] != ']')
+            {
+                if (isdigit(s[i]))
+                {
+                    int k = 0;
+                    while (i < s.size() && isdigit(s[i]))
+                    {
+                        k = k *10 + s[i] - '0';
+                        i++;
+                    }
+                    cout << k<<endl;
+                    i++;
+                    string r = Decode(s, i);
+                    while (k > 0)
+                    {
+                        result += r;
+                  
+                        k--;
+                    }
+                          i++;
+                }
+                else
+                {
+                    result += s[i];
+                    i++;
+                }
+            }
+            return result;
+        }
+    string decodeString(string s)
+    {
+        int i = 0;
+        return Decode(s, i);
+    }
+};
