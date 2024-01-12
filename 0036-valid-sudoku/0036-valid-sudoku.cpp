c288ab0b4f39da428f89009bace8861c721d922b
SAYAN MAITRA <75684139+sayan112@users.noreply.github.com>
2024-01-12 17:16:00 +0530

    Time: 18 ms (61.42%), Space: 21.2 MB (15.30%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -1,37 +1,37 @@
-class Solution {
-public:
-    bool isValidSudoku(vector<vector<char>>& board) {
-        unordered_map<string,int>mp;
-        for(int i=0;i<board.size();i++)
+class Solution
+{
+    public:
+        bool isValidSudoku(vector<vector < char>> &board)
         {
-           for(int j=0;j<board[0].size();j++)
+            unordered_map<string, int> mp;
+            for (int i = 0; i < board.size(); i++)
             {
-               
-               
-                if(board[i][j]=='.')
+                for (int j = 0; j < board[0].size(); j++)
                 {
-                    continue;
-                }
-               else{
-                                 string stRow="row"+to_string(i)+board[i][j];
-               cout << stRow<<" ";
-                string stCol="col"+to_string(j)+board[i][j];
-               cout << stCol<<" ";
-                string stSubBox=to_string(((i/3)*3+j/3))+"Box"+board[i][j];
-               cout << stSubBox <<endl;
-                    
-                   mp[stRow]++;
-                   mp[stCol]++;
-                   mp[stSubBox]++;
-                    if(mp[stRow]==2||mp[stCol]==2||mp[stSubBox]==2)
-                    {
-                        return false;
-                    }
-                   
-               }
 
+                    if (board[i][j] == '.')
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        string stRow = "row" + to_string(i) + board[i][j];
+                       	// cout << stRow<<" ";
+                        string stCol = "col" + to_string(j) + board[i][j];
+                       	// cout << stCol<<" ";
+                        string stSubBox = to_string(((i / 3) *3 + j / 3)) + "Box" + board[i][j];
+                       	// cout << stSubBox <<endl;
+
+                        mp[stRow]++;
+                        mp[stCol]++;
+                        mp[stSubBox]++;
+                        if (mp[stRow] == 2 || mp[stCol] == 2 || mp[stSubBox] == 2)
+                        {
+                            return false;
+                        }
+                    }
+                }
             }
+            return true;
         }
-        return true;
-    }
 };
