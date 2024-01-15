f6019b4aee1e80b553b7cac8a56d27e46ac1d004
SAYAN MAITRA <75684139+sayan112@users.noreply.github.com>
2024-01-15 21:42:46 +0530

    Time: 4 ms (84.13%), Space: 9.6 MB (61.26%) - LeetHub

--- 2558-take-gifts-from-the-richest-pile/2558-take-gifts-from-the-richest-pile.cpp
+++ 2558-take-gifts-from-the-richest-pile/2558-take-gifts-from-the-richest-pile.cpp
@@ -1,36 +1,28 @@
-class Solution {
-public:
-    long long pickGifts(vector<int>& gifts, int k) {
-        vector<int>vec;   
-    priority_queue<int>pq;
-         for(int i =0;i<gifts.size();i++)
-         {
-              pq.push(gifts[i]);
-         }
-       
-          while(pq.size()&&k)
-          {
-              int need = floor(sqrt(pq.top()));
-           
-             
-              pq.pop();
-              k--;
-              pq.push(need);
-              
-          }
-                long long sum=0;
-        while(pq.size())
+class Solution
+{
+    public:
+        long long pickGifts(vector<int> &gifts, int k)
         {
-           
-            sum+= pq.top();
-            pq.pop();
+            vector<int> vec;
+            priority_queue<int> pq;
+            for (int i = 0; i < gifts.size(); i++)
+            {
+                pq.push(gifts[i]);
+            }
+
+            while (pq.size() && k--)
+            {
+                int need = floor(sqrt(pq.top()));
+                pq.pop();
+                pq.push(need);
+            }
+            long long sum = 0;
+            while (pq.size())
+            {
+                sum += pq.top();
+                pq.pop();
+            }
+
+            return sum;
         }
- 
-    
-       
-        return sum;
-        
-        
-  
-    }
 };
