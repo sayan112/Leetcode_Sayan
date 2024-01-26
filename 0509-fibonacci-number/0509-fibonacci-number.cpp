dd2f0b5e674545f0578451baef9d3273340590a7
SAYAN MAITRA <75684139+sayan112@users.noreply.github.com>
2024-01-26 13:49:36 +0530

    Time: 0 ms (100.00%), Space: 7.3 MB (8.20%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,10 +1,13 @@
 class Solution {
 public:
     int fib(int n) {
-        if(n==0||n==1)
-        {
-             return n ;
-        }
-          return fib(n-1)+fib(n-2);
+        vector<int>vec;
+         vec.push_back(0);
+         vec.push_back(1);
+         for(int i=2;i<=n;i++)
+         {
+             vec.push_back(vec[i-1]+vec[i-2]);
+         }
+         return vec[n];
     }
 };
