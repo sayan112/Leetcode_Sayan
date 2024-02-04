e9727423084169ff95e5ae3974f0926984e894c4
SAYAN MAITRA <75684139+sayan112@users.noreply.github.com>
2024-02-04 20:01:45 +0530

    Time: 54 ms (18.90%), Space: 42.3 MB (19.20%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,33 +1,35 @@
-class Solution {
-public:
-     void generateAllCombination (vector<int>& candidates , int idx , int target ,     vector<int>ds , vector<vector<int>>&ans  )
-     {
-         if(idx==candidates.size())
-         {
-             if(target==0)
-             {
-                 ans.push_back(ds);
-             }
-              return ;
-         }
-          if(target>=candidates[idx])
-          {
-                 ds.push_back(candidates[idx]);
-         target-=candidates[idx];
-         generateAllCombination(candidates,idx,target,ds,ans);
-         ds.pop_back();
-          target+=candidates[idx];
-          }
-      
-         generateAllCombination(candidates,idx+1,target,ds,ans);
-         
-         
-     }
-    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        vector<int>ds;
-         int idx =0;
-           vector<vector<int>>ans;
-        generateAllCombination(candidates,idx,target,ds,ans);
-         return ans ;
+class Solution
+{
+    public:
+        void findallcombinations(vector<int> &candidates, vector<vector< int >> &ans, int target, vector< int > vec, int idx)
+        {
+            if (idx >= candidates.size())
+            {
+                if (target == 0)
+                {
+                    ans.push_back(vec);
+                }
+                return;
+            }
+            if (target >= candidates[idx])
+            {
+                vec.push_back(candidates[idx]);
+                target -= candidates[idx];
+                findallcombinations(candidates, ans, target, vec, idx);
+                vec.pop_back();
+                target += candidates[idx];
+            }
+
+            findallcombinations(candidates, ans, target, vec, idx + 1);
+        }
+    vector<vector < int>> combinationSum(vector<int> &candidates, int target)
+    {
+
+        vector<vector < int>> ans;
+        int idx = 0;
+        vector<int> vec;
+
+        findallcombinations(candidates, ans, target, vec, idx);
+        return ans;
     }
 };
