7a061dbed517a8aa7173c641742aaff5bdc7dfc5
SAYAN MAITRA <75684139+sayan112@users.noreply.github.com>
2024-02-09 00:07:48 +0530

    Time: 5 ms (33.52%), Space: 8.9 MB (30.69%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,33 +1,24 @@
-class Solution
-{
-    public:
-        void generateallpermutation(vector<int> &nums, int idx, vector<vector< int >> &ans, vector< bool > &check, vector< int > &ds)
-        {
-            if (ds.size() == nums.size())
-            {
-                ans.push_back(ds);
-                return;
-            }
-            for (int i = 0; i < nums.size(); i++)
-            {
-                if (check[i])
-                {
-                     check[i]=false;
-                    ds.push_back(nums[i]);
-               
-                    generateallpermutation(nums, idx + 1, ans, check, ds);
-                    ds.pop_back();
-                    check[i] = true;
-                }
-            }
-        }
-    vector<vector < int>> permute(vector<int> &nums)
-    {
-        int idx = 0;
-        vector<vector < int>> ans;
-        vector<bool>check(nums.size(), true);
-        vector<int> ds;
-        generateallpermutation(nums, idx, ans, check, ds);
-        return ans;
+class Solution {
+public:
+     void permutaion ( vector<int>&nums ,   vector<vector<int>>&ans , int i  )
+     {
+          if(i==nums.size()-1)
+          {
+               ans.push_back(nums);
+               return ;
+          }
+         for(int j=i;j<nums.size();j++)
+         {
+             swap(nums[j],nums[i]);
+              permutaion ( nums , ans  , i+1);
+                swap(nums[j],nums[i]);
+         }
+         
+     }
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>>ans;
+        int  idx =0;
+         permutaion ( nums , ans  , idx);
+         return ans;
     }
 };
