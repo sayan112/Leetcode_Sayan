827fa43c294cae1bda9395a2039865953b7d85a4
SAYAN MAITRA <75684139+sayan112@users.noreply.github.com>
2024-02-16 20:04:52 +0530

    Time: 233 ms (31.66%), Space: 175.7 MB (8.02%) - LeetHub

--- 1609-even-odd-tree/1609-even-odd-tree.cpp
+++ 1609-even-odd-tree/1609-even-odd-tree.cpp
@@ -1,90 +1,95 @@
 /**
- *Definition for a binary tree node.
- *struct TreeNode {
- *  int val;
- *  TreeNode * left;
- *  TreeNode * right;
- *  TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *  TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- *};
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
  */
-class Solution
-{
-    public:
-
-        bool isEvenOddTree(TreeNode *root)
-        {
-            vector<vector < int>> ans;
-
-            queue<TreeNode*> q;
-            q.push(root);
-
-            while (!q.empty())
+class Solution {
+public:
+    bool checkforincreaseordecrease ( vector<int>&vec , int count )
+    {
+         if(count%2==0)
+         {
+              if(vec[0]%2==0)
+                 {
+                     return false;
+                 }
+             
+             for(int i=1;i<vec.size();i++)
+             {
+                 if(vec[i]%2==0)
+                 {
+                     return false;
+                 }
+              if(vec[i-1]>=vec[i])
+              {
+                   return false;
+              }
+             }
+              
+         }
+        else {
+            
+            if(vec[0]%2!=0)
             {
-                vector<int> vec;
-                int size = q.size();
-                for (int i = 0; i < size; i++)
-                {
-                    TreeNode *node = q.front();
-                    q.pop();
-                    vec.push_back(node->val);
-                    if (node->left)
-                    {
-                        q.push(node->left);
-                    }
-                    if (node->right)
-                    {
-                        q.push(node->right);
-                    }
-                }
-                ans.push_back(vec);
+                return false;
             }
-
-            int check = 0;
-
-            for (auto elem1: ans)
-            {
-
-                if (check == 0)
-                {
-                    check = 1;
-                    for (auto elem: elem1)
-                    {
-                        if (elem % 2 == 0)
-                        {
-                            return false;
-                        }
-                    }
-                    for (int i = 1; i < elem1.size(); i++)
-                    {
-                        if (elem1[i] <= elem1[i - 1])
-                        {
-                            return false;
-                        }
-                    }
-                }
-                else
-                {
-
-                    check = 0;
-                    for (auto elem: elem1)
-                    {
-                        if (elem % 2 != 0)
-                        {
-                            return false;
-                        }
-                    }
-                    for (int i = 1; i < elem1.size(); i++)
-                    {
-                        if (elem1[i] >= elem1[i - 1])
-                        {
-
-                            return false;
-                        }
-                    }
-                }
-            }
-            return true;
+              for(int i=1;i<vec.size();i++)
+             {
+                    if(vec[i]%2!=0)
+                 {
+                     return false;
+                 }
+              if(vec[i-1]<=vec[i])
+              {
+                   return false;
+              }
+             }
         }
+         return true;
+    }
+    bool isEvenOddTree(TreeNode* root) {
+         queue<TreeNode*>q;
+         q.push(root);
+         vector<vector<int>>ans;
+         while(q.size())
+         {
+              int size = q.size();
+              vector<int>vec;
+             for(int i =0;i<size;i++)
+             {
+                 TreeNode* node=q.front();
+                  vec.push_back(node->val);
+                 q.pop();
+                  if(node->left)
+                  {
+                      q.push(node->left);
+                  }
+                   if(node->right)
+                  {
+                      q.push(node->right);
+                  }
+                
+             }
+              ans.push_back(vec);
+             
+         }
+         int count =0;
+         for(auto elem : ans)
+         {
+             
+        if(!checkforincreaseordecrease(elem,count))
+        {
+             return false;
+        }
+             count++;
+             
+         }
+         return true;
+    }
 };
