4a5a58002aca6f17cb3c4ce83bf3b2852bc745a4
SAYAN MAITRA <75684139+sayan112@users.noreply.github.com>
2024-02-17 10:21:40 +0530

    Time: 10 ms (39.68%), Space: 19.7 MB (94.56%) - LeetHub

--- 0112-path-sum/0112-path-sum.cpp
+++ 0112-path-sum/0112-path-sum.cpp
@@ -1,43 +1,38 @@
 /**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
+ *Definition for a binary tree node.
+ *struct TreeNode {
+ *   int val;
+ *   TreeNode * left;
+ *   TreeNode * right;
+ *   TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *   TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *   TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *};
  */
-class Solution {
-public:
-    
-     bool targetSumAchivedOrNot(TreeNode* root, int targetSum)
-     {
-         if(!root)
-         {
-return false;
-         }
-          if(root->left==NULL && root->right==NULL)
-          {
-               if((targetSum-root->val)==0)
-               {
+class Solution
+{
+    public:
+        bool checkpathsum(TreeNode *root, int sum, int targetSum)
+        {
+            if (!root)
+            {
+                 
+                return false;
+            }
+            if (!root->left && !root->right) {
+                sum+=root->val;
+                if(targetSum==sum)
+                {
                     return true;
-               }
-          }
-         
-         
-         targetSum-=root->val;
-   bool check1=  targetSumAchivedOrNot(root->left , targetSum) ;
-         bool check2= targetSumAchivedOrNot(root->right , targetSum);
-          return check1|| check2;
-         
-     }
-    bool hasPathSum(TreeNode* root, int targetSum) {
-         if(!root)
-         {
-             return false;
-         }
-       return  targetSumAchivedOrNot(root,targetSum);
+                }
+               
+            }
+sum+=root->val;
+           return  checkpathsum(root->left, sum, targetSum) ||   checkpathsum(root->right, sum, targetSum);
+        }
+    bool hasPathSum(TreeNode *root, int targetSum)
+    {
+        int sum = 0;
+      return  checkpathsum(root, sum, targetSum);
     }
 };
